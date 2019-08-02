50c5f0f9a040928dd7550cd10b6b6fae81345489
zengbin93 <zeng_bin8888@163.com>
2019-08-02 22:24:17 +0800

    modify get_kline for index

--- chan/a/utils.py
+++ chan/a/utils.py
@@ -3,7 +3,7 @@
 import tushare as ts
 
 
-def get_kline(ts_code, start_date, end_date, freq='30min'):
+def get_kline(ts_code, start_date, end_date, freq='30min', asset='E'):
     """获取指定级别的前复权K线
 
     :param ts_code: str
@@ -14,16 +14,19 @@ def get_kline(ts_code, start_date, end_date, freq='30min'):
         日期，如 20190601
     :param end_date: str
         日期，如 20190610
+    :param asset: str
+        交易资产类型，可选值 E股票 I沪深指数 C数字货币 FT期货 FD基金 O期权 CB可转债（v1.2.39），默认E
     :return: pd.DataFrame
         columns = ["symbol", "dt", "open", "close", "high", "low", "vol"]
 
     example:
+    >>> from chan.a.utils import get_kline
     >>> get_kline(ts_code='600122.SH', start_date='20190601', end_date='20190610', freq='30min')
     """
     # https://tushare.pro/document/2?doc_id=109
     df = ts.pro_bar(ts_code=ts_code, freq=freq,
                     start_date=start_date, end_date=end_date,
-                    adj='qfq', asset='E')
+                    adj='qfq', asset=asset)
 
     # 统一 k 线数据格式为 6 列，分别是 ["symbol", "dt", "open", "close", "high", "low", "vr"]
     if "min" in freq:
@@ -43,13 +46,12 @@ def get_kline(ts_code, start_date, end_date, freq='30min'):
 
 
 def get_realtime_kline(ts_code, freq="5min"):
-    """实时获取分钟K线
+    """实时获取分钟K线（仅适用于A股股票）
 
     :param ts_code: str
         tushare 股票代码，如 600122.SH
     :param freq: str
         K线周期，分钟级别，可选值 5min 15min 30min 60min
-
     :return: pd.DataFrame
         columns = ["symbol", "dt", "open", "close", "high", "low", "vol"]
 
