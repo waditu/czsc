c2e2a28ffc9846afae58fb3fcaf67b9f20f87529
zengbin93 <zeng_bin8888@163.com>
2019-08-17 21:33:08 +0800

    add macd view

--- chan/__init__.py
+++ chan/__init__.py
@@ -2,11 +2,12 @@
 from . import a
 from .web.vis_kline import kline_viewer
 from .utils import cache_path, clean_cache
-from .utils import preprocess, find_bi, find_xd, kline_status, ma, macd
+from .utils import kline_status, ma, macd
+from .analyze import preprocess, find_fx, find_bi, find_xd
 
 
 author = "zengbin93"
-version = "0.0.2"
+version = "0.0.3"
 email = "zeng_bin8888@163.com"
 
 
