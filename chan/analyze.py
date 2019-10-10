b5af7d34e7060d3181fb0c5084c96ba8f8df3e58
zengbin93 <zeng_bin8888@163.com>
2019-10-10 21:28:05 +0800

    fix xd bug

--- chan/analyze.py
+++ chan/analyze.py
@@ -1,5 +1,7 @@
 # coding: utf-8
+
 import pandas as pd
+from copy import deepcopy
 
 
 def preprocess(kline):
@@ -7,231 +9,284 @@ def preprocess(kline):
 
     :param kline: pd.DataFrame
         K线，columns = ["symbol", "dt", "open", "close", "high", "low", "vol"]
-    :return: pd.DataFrame
+    :return: list of dict
+
     """
-    kline['high_m'] = None
-    kline['low_m'] = None
+    k_new = []
 
-    # 首行处理
-    last_h = kline.loc[0, 'high']
-    last_l = kline.loc[0, 'low']
-
-    if last_h >= kline.loc[1, 'high']:
-        direction = 0  # 下跌
-    else:
-        direction = 1  # 上涨
+    first_row = kline.iloc[0].to_dict()
+    last = deepcopy(first_row)
+    k_new.append(first_row)
 
     for i, row in kline.iterrows():
+        # first row
+        if i == 0:
+            continue
+
+        # update direction
+        if last['high'] >= k_new[-1]['high']:
+            direction = 'up'
+        else:
+            direction = 'down'
+
+        # 包含关系处理
+        row = row.to_dict()
         cur_h, cur_l = row['high'], row['low']
+        last_h, last_l = last['high'], last['low']
 
         # 左包含 or 右包含
         if (cur_h <= last_h and cur_l >= last_l) or (cur_h >= last_h and cur_l <= last_l):
-            if direction == 0:
-                last_h = min(last_h, cur_h)
-                last_l = min(last_l, cur_l)
-            elif direction == 1:
+            # 有包含关系，按方向分别处理
+            if direction == "up":
                 last_h = max(last_h, cur_h)
                 last_l = max(last_l, cur_l)
+
+            elif direction == "down":
+                last_h = min(last_h, cur_h)
+                last_l = min(last_l, cur_l)
+
             else:
                 raise ValueError
 
-            # 尾行更新 high low
-            if i == len(kline) - 1:
-                kline.loc[i, 'high_m'] = last_h
-                kline.loc[i, 'low_m'] = last_l
-            continue
-
-        # 当前行与上一行之间无包含关系，更新上一行 high low
-        kline.loc[i-1, 'high_m'] = last_h
-        kline.loc[i-1, 'low_m'] = last_l
-
-        # 更新 direction, last_h, last_l
-        if last_h >= cur_h:
-            direction = 0  # 下跌
+            last = deepcopy(row)
+            last['high'] = last_h
+            last['low'] = last_l
         else:
-            direction = 1  # 上涨
+            # 无包含关系，更新 K 线
+            k_new.append(last)
+            last = deepcopy(row)
 
-        last_h = cur_h
-        last_l = cur_l
+        if i == len(kline):
+            k_new.append(last)
 
-    # 根据包含关系的检查结果，生成新的 K 线数据
-    kline_new = kline[['symbol', 'dt', 'open', 'close', 'high_m', 'low_m', 'vol']]
-    kline_new = kline_new.rename({"high_m": "high", "low_m": "low"}, axis='columns')
-    kline_new = kline_new.dropna(subset=['high', 'low'])
-    kline_new = kline_new.reset_index(drop=True)
-
-    return kline_new
+    # print('k_new nums:', len(k_new))
+    return k_new
 
 
-def find_fx(kline):
-    """找出全部分型
+def find_fx(k_new):
+    """查找顶分型和底分型"""
+    k1 = k_new[0]
+    k2 = k_new[1]
 
-    0 - 顶分型
-    1 - 底分型
+    k_fx = []
+    for k in k_new[2:]:
+        if k2['high'] > k1['high'] and k2['high'] > k['high']:
+            k2['fx_mark'] = 'g'
+            k2['fx'] = k2['high']
+        elif k2['low'] < k1['low'] and k2['low'] < k['low']:
+            k2['fx_mark'] = 'd'
+            k2['fx'] = k2['low']
 
-    一个分型能否成为笔的一部分，至少需要看到后面两个分型的情况才能确定。
+        k_fx.append(k2)
+        k1, k2, = k2, k
 
-    :param kline: pd.DataFrame
-        经过预处理，去除了包含关系的 K 线，
-        columns = ["symbol", "dt", "open", "close", "high", "low", "vol"]
-    :return: kline: pd.DataFrame
-    """
-    kline['fx'] = None
-    kline['fx_mark'] = None
-    last_fx_mark = 0
+    # print("fx nums:", len(k_fx))
+    return k_fx
 
-    for i in range(1, len(kline) - 1):
-        data = kline.iloc[i-1: i+2]
-        row = kline.iloc[i]
-        if max(data['high']) == row['high'] and last_fx_mark == 1:
-            kline.loc[row.name, 'fx_mark'] = 0
-            kline.loc[row.name, 'fx'] = kline.loc[i, 'high']
-            last_fx_mark = 0
-        elif min(data['low']) == row['low'] and last_fx_mark == 0:
-            kline.loc[row.name, 'fx_mark'] = 1
-            kline.loc[row.name, 'fx'] = kline.loc[i, 'low']
-            last_fx_mark = 1
+
+def find_bi(k_fx):
+    """查找分型标记"""
+    k_bi = []
+
+    potential = None
+    k_count = 0
+
+    for k in k_fx:
+        # print(k)
+        if not potential:
+            # 查找第一个潜在笔分型点
+            if "fx_mark" in k.keys():
+                potential = deepcopy(k)
+            else:
+                continue
+
+        # 验证各分型点是否能够作为笔分型点
+        if "fx_mark" in k.keys():
+            if k['fx_mark'] == potential['fx_mark']:
+                if k['fx_mark'] == "g" and k['fx'] > potential['fx']:
+                    potential = deepcopy(k)
+                elif k['fx_mark'] == "d" and k['fx'] < potential['fx']:
+                    potential = deepcopy(k)
+                else:
+                    continue
+                k_count = 0
+            else:
+                if k_count >= 3:
+                    potential['bi'] = potential['fx']
+                    k_bi.append(potential)
+                    # print("potential old: ", k_count, potential)
+                    k_count = 0
+                    potential = deepcopy(k)
+                    # print("potential new: ", k_count, potential)
+                # k_count = 0
         else:
-            continue
-    return kline
+            # print(k_count, k)
+            k_count += 1
+
+    potential['bi'] = potential['fx']
+    k_bi.append(potential)
+    # print("bi nums:", len(k_bi))
+    return k_bi
 
 
-def find_bi(kline):
-    """笔标记：确定哪些分型能够成为笔的一部分
-
-    一个分型能否成为笔的一部分，至少需要看到后面两个分型的情况才能确定。
-
-    0 - 顶分型
-    1 - 底分型
-
-    :param kline: pd.DataFrame
-        经过预处理，去除了包含关系，并标记出所有分型的K线
-        columns = ["symbol", "dt", "open", "close", "high", "low", "vol", 'fx]
-    :return: kline: pd.DataFrame
-    """
-
-    fx_list = []
-    for i, row in kline.iterrows():
-        if row['fx_mark'] in [0, 1]:
-            row_dict = row.to_dict()
-            row_dict['id'] = i
-            fx_list.append(row_dict)
-
-    if fx_list[0]['fx_mark'] != 1:
-        fx_list.pop(0)
-
-    assert fx_list[0]['fx_mark'] == 1, "第一个分型标记不是0"
-
-    valid_fx_list = []
-    potential_valid = None
-
+def find_xd(k_bi):
+    """查找线段标记"""
+    k_xd = []
     i = 0
-    # 一旦确定一个有效的分型，则一个不同类型的潜在有效分型也得到确认
-    while i < len(fx_list)-1:
-        if len(valid_fx_list) == 0:
-            # 查找第一个有效的底分型
-            if fx_list[i]['fx_mark'] == 1 and fx_list[i+1]['id'] - fx_list[i]['id'] > 3:
-                valid_fx_list.append(fx_list[i])
-                potential_valid = fx_list[i+1]
-        else:
-            # 对每一个潜在有效分型，只有当一个相距至少4根K线的不同类型分型出现后，才能确认其有效性
-            if fx_list[i]['fx_mark'] == potential_valid['fx_mark']:
-                # 更新 potential valid
-                if potential_valid['fx_mark'] == 0 and fx_list[i]['fx'] > potential_valid['fx']:
-                    potential_valid = fx_list[i]
-                elif potential_valid['fx_mark'] == 1 and fx_list[i]['fx'] < potential_valid['fx']:
-                    potential_valid = fx_list[i]
+    potential = deepcopy(k_bi[i])
+
+    while i < len(k_bi)-3:
+        k1, k2, k3 = k_bi[i+1], k_bi[i+2], k_bi[i+3]
+
+        if potential['fx_mark'] == "d":
+            assert k2['fx_mark'] == 'd'
+            if k3['bi'] < k1['bi']:
+                potential['xd'] = potential['bi']
+                k_xd.append(potential)
+                i += 1
+                potential = deepcopy(k_bi[i])
             else:
-                # 确认 potential valid
-                if fx_list[i]['id'] - potential_valid['id'] > 3:
-                    valid_fx_list.append(potential_valid)
-                    potential_valid = fx_list[i]
-        i += 1
+                i += 2
 
-    # 整理结果输出
-    for i, fx in enumerate(valid_fx_list):
-        fx['bi_mark'] = int(i)
-        if fx['fx_mark'] == 0:
-            fx['bi'] = fx['high']
+        elif potential['fx_mark'] == "g":
+            assert k2['fx_mark'] == 'g'
+            if k3['bi'] > k1['bi']:
+                potential['xd'] = potential['bi']
+                k_xd.append(potential)
+                i += 1
+                potential = deepcopy(k_bi[i])
+            else:
+                i += 2
+
         else:
-            fx['bi'] = fx['low']
+            raise ValueError
 
-    fx_df = pd.DataFrame(valid_fx_list)
-    fx_df = fx_df[['dt', 'bi', 'bi_mark']]
-    kline = kline.merge(fx_df, how='left', on='dt')
-    return kline
+    potential['xd'] = potential['bi']
+    k_xd.append(potential)
+    # print("xd nums:", len(k_xd))
+    # 待完善：如果两个线段标记之间无有效笔标记，则这两个线段标记无效。
+
+    return k_xd
 
 
-def find_xd(kline):
-    """线段查找。输入：确定了分型的 K 线；输出：加入线段查找结果的 K 线
+def find_zs(k_xd):
+    """查找中枢"""
+    k_zs = []
+    zs_xd = []
 
-    :param kline: pd.DataFrame
-        K线，columns = ["symbol", "dt", "open", "close", "high", "low", "vol", 'bi', 'bi_mark', 'fx', 'fx_mark']
-    :return: pd.DataFrame
-        K线，columns = ['symbol', 'dt', 'open', 'close', 'high', 'low', 'vol', 'fx', 'fx_mark', 'bi', 'bi_mark',
-                       'xd', 'xd_mark']
-    """
-    kline_new = kline[["dt", 'bi', 'bi_mark', 'fx', 'fx_mark']]
-    kline_new = kline_new.dropna()
-    kline_new = kline_new.reset_index(drop=True)
-    assert kline_new.iloc[0]['fx_mark'] == 1, "分型标记必选从 1 开始"
-
-    # 利用向上笔查找潜在的线段顶分型，利用向下笔查找潜在的线段底分型
-    kline_new['xd_fx'] = None
-    kline_new['xd'] = None
-    for i in range(1, len(kline_new)-2):
-        last = kline_new.iloc[i-2]['bi']
-        cur = kline_new.iloc[i]['bi']
-        next_ = kline_new.iloc[i+2]['bi']
-        if i % 2 == 0:
-            if min([last, cur, next_]) == cur:
-                kline_new.loc[i, 'xd_fx'] = 1
-                kline_new.loc[i, 'xd'] = cur
-        else:
-            if max([last, cur, next_]) == cur:
-                kline_new.loc[i, 'xd_fx'] = 0
-                kline_new.loc[i, 'xd'] = cur
-
-    # 确认线段分型的有效性：至少存在一个反方向的一笔
-    potential_valid = None
-    valid_list = []
-
-    for i, row in kline_new.iterrows():
-        if row['xd_fx'] not in [0, 1]:
+    for i in range(len(k_xd)-1):
+        if len(zs_xd) < 4:
+            zs_xd.append(k_xd[i])
             continue
 
-        if not potential_valid:
-            # 找到第一个潜在的有效线段分型
-            potential_valid = row.to_dict()
-            potential_valid['id'] = i
+        zs_d = max([x['xd'] for x in zs_xd if x['fx_mark'] == 'd'])
+        zs_g = min([x['xd'] for x in zs_xd if x['fx_mark'] == 'g'])
+        # zs = (zs_d, zs_g)
+        xd_p1 = zs_xd[-1]['xd']
+        xd_p2 = k_xd[i]['xd']
+        if zs_g > xd_p2 > zs_d:
+            zs_xd.append(k_xd[i])
         else:
-            # 对潜在分型进行相应的判断
-            if row['xd_fx'] == potential_valid['xd_fx']:
-                # update 潜在顶分型
-                if potential_valid['xd_fx'] == 0 and row['xd'] > potential_valid['xd']:
-                    potential_valid = row.to_dict()
-                    potential_valid['id'] = i
-                # update 潜在底分型
-                if potential_valid['xd_fx'] == 1 and row['xd'] < potential_valid['xd']:
-                    potential_valid = row.to_dict()
-                    potential_valid['id'] = i
+            if xd_p1 > zs_g and xd_p2 > zs_g:
+                # 线段在中枢上方结束，形成三买
+                k_zs.append({'zs': (zs_d, zs_g), "zs_xd": deepcopy(zs_xd)})
+                zs_xd = [k_xd[i]]
+            elif xd_p1 < zs_g and xd_p2 < zs_g:
+                # 线段在中枢下方结束，形成三卖
+                k_zs.append({'zs': (zs_d, zs_g), "zs_xd": deepcopy(zs_xd)})
+                zs_xd = [k_xd[i]]
             else:
-                # 判断潜在分型的有效性
-                # 取两个不同类型分型之间的数据，不包括两个分型点
-                bi_part = kline_new.iloc[potential_valid['id']+1:i]
-                if sum(bi_part['fx_mark']) >= 1:  # 两个有效线段分型之间至少存在一笔
-                    valid_list.append(potential_valid)
+                zs_xd.append(k_xd[i])
 
-                potential_valid = row.to_dict()
-                potential_valid['id'] = i
+    return k_zs
 
-    valid_df = pd.DataFrame(valid_list)
-    valid_df['xd_mark'] = [x for x in range(len(valid_df))]
 
-    valid_df = valid_df[['dt', 'xd', 'xd_mark']]
-    kline = kline.merge(valid_df, how='left', on='dt')
-    print(kline.columns)
+def kline_analyze(kline):
+    """使用缠论分析 K 线"""
+    k_new = preprocess(kline)
+    k_fx = find_fx(k_new)
+    k_bi = find_bi(k_fx)
+    k_xd = find_xd(k_bi)
+
+    df_fx = pd.DataFrame(k_fx)[['dt', 'fx_mark', 'fx']]
+    kline = kline.merge(df_fx, how='left', on='dt')
+
+    df_bi = pd.DataFrame(k_bi)[['dt', 'bi']]
+    kline = kline.merge(df_bi, how='left', on='dt')
+
+    df_xd = pd.DataFrame(k_xd)[['dt', 'xd']]
+    kline = kline.merge(df_xd, how='left', on='dt')
     return kline
+
+
+class KlineAnalyze(object):
+    def __init__(self, kline):
+        self.kline = kline
+        self.k_new = preprocess(self.kline)
+        self.k_fx = find_fx(self.k_new)
+        self.k_bi = find_bi(self.k_fx)
+        self.k_xd = find_xd(self.k_bi)
+
+    @property
+    def chan_result(self):
+        k = deepcopy(self.kline)
+        df_fx = pd.DataFrame(self.k_fx)[['dt', 'fx_mark', 'fx']]
+        k = k.merge(df_fx, how='left', on='dt')
+
+        df_bi = pd.DataFrame(self.k_bi)[['dt', 'bi']]
+        k = k.merge(df_bi, how='left', on='dt')
+
+        df_xd = pd.DataFrame(self.k_xd)[['dt', 'xd']]
+        k = k.merge(df_xd, how='left', on='dt')
+        return k
+
+    @property
+    def status_bi(self):
+        k1, k2, k3 = self.k_new[-1], self.k_new[-2], self.k_new[-3]
+        if k1['high'] > k2['high'] > k3['high']:
+            s = "向上笔延伸中"
+        elif k1['low'] < k2['low'] < k3['low']:
+            s = "向下笔延伸中"
+        elif max([k1['high'], k2['high'], k3['high']]) == k2['high']:
+            s = "顶分型构造中"
+        elif min([k1['low'], k2['low'], k3['low']]) == k2['low']:
+            s = "底分型构造中"
+        else:
+            raise ValueError
+        return s
+
+    @property
+    def status_xd(self):
+        last = self.k_xd[-1]
+        if last['fx_mark'] == 'g':
+            s = "向下线段延伸中"
+        elif last['fx_mark'] == 'd':
+            s = "向上线段延伸中"
+        else:
+            raise ValueError
+        return s
+
+    @property
+    def status_zs(self):
+        if len(self.k_xd) < 5:
+            return "没有中枢"
+
+        xd1 = (self.k_xd[-5], self.k_xd[-4])
+        xd2 = (self.k_xd[-4], self.k_xd[-3])
+        # xd3 = (self.k_xd[-3], self.k_xd[-2])
+        xd4 = (self.k_xd[-2], self.k_xd[-1])
+        zs = (min([xd2[0]['fx'], xd2[1]['fx']]), max([xd2[0]['fx'], xd2[1]['fx']]))
+
+        if xd1[0]['fx_mark'] == "g" and xd4[1]['fx'] < zs[0]:
+            # 找第三卖点是否形成
+            s = '中枢下移'
+        elif xd1[0]['fx_mark'] == "d" and xd4[1]['fx'] > zs[1]:
+            s = '中枢上移'
+        else:
+            s = '中枢震荡'
+        return s
+
+
 
 
 
