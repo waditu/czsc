6d0a2684e1fa4a72b3cd8c133787b5fd8005d709
zengbin93 <zeng_bin8888@163.com>
2020-03-14 16:14:24 +0800

    新增仿真结果可视化

--- examples/trade_simulator.py
+++ examples/trade_simulator.py
@@ -9,7 +9,13 @@ import traceback
 import pandas as pd
 import tushare as ts
 from datetime import datetime, timedelta
-from chan import SolidAnalyze
+from chan import SolidAnalyze, KlineAnalyze, macd
+from pyecharts import options as opts
+from pyecharts.commons.utils import JsCode
+from snapshot_selenium import snapshot
+from pyecharts.render import make_snapshot
+from pyecharts.charts import Kline, Line, Bar, Grid, Scatter
+
 
 # 首次使用，需要在这里设置你的 tushare token，用于获取数据；在同一台机器上，tushare token 只需要设置一次
 # 没有 token，到 https://tushare.pro/register?reg=7 注册获取
@@ -202,9 +208,10 @@ def kline_simulator(ts_code, trade_dt, asset="E", count=5000):
         yield new_klines
 
 
-def trade_simulator(ts_code, end_date, start_date=None, days=3, asset="E", watch_interval=5):
+def trade_simulator(ts_code, end_date, file_bs, start_date=None, days=3, asset="E", watch_interval=5):
     """单只标的类实盘模拟，研究买卖点变化过程
 
+    :param file_bs:
     :param ts_code: str
         标的代码，如 300033.SZ
     :param end_date: str
@@ -259,10 +266,267 @@ def trade_simulator(ts_code, end_date, start_date=None, days=3, asset="E", watch
         df = df.drop_duplicates(['出现时间', '基准价格', '操作提示', '标的代码'])
         cols = ['标的代码', '操作提示', '交易时间', '交易价格', '交易级别', '出现时间', '基准价格', '其他信息']
         df = df[cols]
-        df.to_excel(f"{ts_code}买卖点变化过程.xlsx", index=False)
+        df.to_excel(file_bs, index=False)
         start_date += timedelta(days=1)
 
 
-if __name__ == '__main__':
-    trade_simulator(ts_code='603305.SH', end_date='20200312', days=10, asset="E", watch_interval=5)
+def draw(df, file_png="test.png"):
+    x = df.dt.to_list()
+    kline = (
+        Kline()
+            .add_xaxis(xaxis_data=x)
+            .add_yaxis(
+            series_name="",
+            y_axis=df[['open', 'close', 'low', 'high']].values.tolist(),
+            itemstyle_opts=opts.ItemStyleOpts(
+                color="#ef232a",
+                color0="#14b143",
+                border_color="#ef232a",
+                border_color0="#14b143",
+            ),
+        )
+            .set_series_opts(
+            markarea_opts=opts.MarkAreaOpts(is_silent=True)
+        )
+            .set_global_opts(
+            title_opts=opts.TitleOpts(title="K线周期图表", pos_left="0"),
+            xaxis_opts=opts.AxisOpts(
+                type_="category",
+                is_scale=True,
+                boundary_gap=False,
+                axisline_opts=opts.AxisLineOpts(is_on_zero=False),
+                splitline_opts=opts.SplitLineOpts(is_show=False),
+                split_number=20,
+                min_="dataMin",
+                max_="dataMax",
+            ),
+            yaxis_opts=opts.AxisOpts(
+                is_scale=True, splitline_opts=opts.SplitLineOpts(is_show=True),
+                axislabel_opts=opts.LabelOpts(is_show=True, position="inside")
+            ),
+            tooltip_opts=opts.TooltipOpts(trigger="axis", axis_pointer_type="line"),
+            datazoom_opts=[
+                opts.DataZoomOpts(
+                    is_show=False, type_="inside", xaxis_index=[0, 0], range_end=100
+                ),
+                opts.DataZoomOpts(
+                    is_show=True, xaxis_index=[0, 1], pos_top="97%", range_end=100
+                ),
+                opts.DataZoomOpts(is_show=False, xaxis_index=[0, 2], range_end=100),
+            ],
+            # 三个图的 axis 连在一块
+            axispointer_opts=opts.AxisPointerOpts(
+                is_show=True,
+                link=[{"xAxisIndex": "all"}],
+                label=opts.LabelOpts(background_color="#777"),
+            ),
+        )
+    )
 
+    kline_line = (
+        Line()
+            .add_xaxis(xaxis_data=x)
+            .add_yaxis(
+            series_name="笔",
+            y_axis=df.bi.tolist(),
+            is_smooth=False,
+            is_connect_nones=True,
+            symbol='diamond',
+            symbol_size=8,
+            linestyle_opts=opts.LineStyleOpts(opacity=1, type_='dotted', width=1.5),
+            label_opts=opts.LabelOpts(is_show=False),
+        )
+            .add_yaxis(
+            series_name="线段",
+            y_axis=df.xd.tolist(),
+            is_smooth=False,
+            is_connect_nones=True,
+            symbol='triangle',
+            symbol_size=12,
+            linestyle_opts=opts.LineStyleOpts(opacity=1, type_='solid', width=1.5),
+            label_opts=opts.LabelOpts(is_show=True, position='right'),
+        )
+            .set_global_opts(
+            xaxis_opts=opts.AxisOpts(
+                type_="category",
+                grid_index=1,
+                axislabel_opts=opts.LabelOpts(is_show=False),
+            ),
+            yaxis_opts=opts.AxisOpts(
+                grid_index=1,
+                split_number=3,
+                axisline_opts=opts.AxisLineOpts(is_on_zero=False),
+                axistick_opts=opts.AxisTickOpts(is_show=False),
+                splitline_opts=opts.SplitLineOpts(is_show=False),
+                axislabel_opts=opts.LabelOpts(is_show=True, position="inside"),
+            ),
+        )
+    )
+    # Overlap Kline + Line
+    overlap_kline_line = kline.overlap(kline_line)
+
+    df1 = df[df['基准价格'] > 0]
+    c = (
+        Scatter()
+            .add_xaxis(df1['出现时间'].to_list())
+            .add_yaxis(
+            "买卖点",
+            df1['基准价格'].to_list(),
+            label_opts=opts.LabelOpts(
+                is_show=True,
+                position="left",
+                formatter=JsCode(
+                    "function(params){return bsName[params.dataIndex][0];}"
+                )
+            ),
+        ))
+    overlap_kline_line = overlap_kline_line.overlap(c)
+
+    # draw volume
+    bar_1 = (
+        Bar()
+            .add_xaxis(xaxis_data=x)
+            .add_yaxis(
+            series_name="Volumn",
+            yaxis_data=df.vol.tolist(),
+            xaxis_index=1,
+            yaxis_index=1,
+            label_opts=opts.LabelOpts(is_show=False),
+            itemstyle_opts=opts.ItemStyleOpts(
+                color=JsCode(
+                    """
+                function(params) {
+                    var colorList;
+                    if (barData[params.dataIndex][1] > barData[params.dataIndex][0]) {
+                        colorList = '#ef232a';
+                    } else {
+                        colorList = '#14b143';
+                    }
+                    return colorList;
+                }
+                """
+                )
+            ),
+        )
+            .set_global_opts(
+            xaxis_opts=opts.AxisOpts(
+                type_="category",
+                grid_index=1,
+                axislabel_opts=opts.LabelOpts(is_show=False),
+            ),
+            yaxis_opts=opts.AxisOpts(
+                axislabel_opts=opts.LabelOpts(is_show=True, position='inside')
+            ),
+            legend_opts=opts.LegendOpts(is_show=False),
+        )
+    )
+
+    # Bar-2 (Overlap Bar + Line)
+    bar_2 = (
+        Bar()
+            .add_xaxis(xaxis_data=x)
+            .add_yaxis(
+            series_name="MACD",
+            yaxis_data=df.macd.tolist(),
+            xaxis_index=2,
+            yaxis_index=2,
+            label_opts=opts.LabelOpts(is_show=False),
+            itemstyle_opts=opts.ItemStyleOpts(
+                color=JsCode(
+                    """
+                        function(params) {
+                            var colorList;
+                            if (params.data >= 0) {
+                              colorList = '#ef232a';
+                            } else {
+                              colorList = '#14b143';
+                            }
+                            return colorList;
+                        }
+                        """
+                )
+            ),
+        )
+            .set_global_opts(
+            xaxis_opts=opts.AxisOpts(
+                type_="category",
+                grid_index=2,
+                axislabel_opts=opts.LabelOpts(is_show=False),
+            ),
+            yaxis_opts=opts.AxisOpts(
+                grid_index=2,
+                split_number=4,
+                axisline_opts=opts.AxisLineOpts(is_on_zero=False),
+                axistick_opts=opts.AxisTickOpts(is_show=False),
+                splitline_opts=opts.SplitLineOpts(is_show=False),
+                axislabel_opts=opts.LabelOpts(is_show=True, position="inside"),
+            ),
+            legend_opts=opts.LegendOpts(is_show=False),
+        )
+    )
+
+    line_2 = (
+        Line()
+            .add_xaxis(xaxis_data=x)
+            .add_yaxis(
+            series_name="DIF",
+            y_axis=df['diff'].tolist(),
+            xaxis_index=2,
+            yaxis_index=2,
+            label_opts=opts.LabelOpts(is_show=False),
+        )
+            .add_yaxis(
+            series_name="DEA",
+            y_axis=df['dea'].tolist(),
+            xaxis_index=2,
+            yaxis_index=2,
+            label_opts=opts.LabelOpts(is_show=False),
+        )
+            .set_global_opts(legend_opts=opts.LegendOpts(is_show=False))
+    )
+
+    # draw MACD
+    overlap_bar_line = bar_2.overlap(line_2)
+
+    # 最后的 Grid
+    grid_chart = Grid(init_opts=opts.InitOpts(width="1400px", height="680px"))
+    grid_chart.add_js_funcs("var barData = {}".format(df[['open', 'close', 'low', 'high']].values.tolist()))
+    grid_chart.add_js_funcs("var bsName = {}".format(df1[["操作提示", "基准价格"]].values.tolist()))
+    grid_chart.add(
+        overlap_kline_line,
+        grid_opts=opts.GridOpts(pos_left="3%", pos_right="1%", height="60%"),
+    )
+    grid_chart.add(
+        bar_1,
+        grid_opts=opts.GridOpts(pos_left="3%", pos_right="1%", pos_top="71%", height="10%"),
+    )
+    grid_chart.add(
+        overlap_bar_line,
+        grid_opts=opts.GridOpts(pos_left="3%", pos_right="1%", pos_top="82%", height="14%"),
+    )
+    file_html = file_png.replace(".png", '.html')
+    make_snapshot(snapshot, grid_chart.render(path=file_html), file_png)
+
+
+def check_trade(ts_code, freq, file_bs, file_png="bs.png"):
+    """在图上画出买卖点"""
+    bs = pd.read_excel(file_bs)
+    bs['f'] = bs['操作提示'].apply(lambda x: 1 if freq_map[freq] in x and "同级别" not in x else 0)
+    bs = bs[bs.f == 1]
+    df = get_kline(ts_code, freq=freq, end_date="20200314", asset='E')
+    ka = KlineAnalyze(df)
+    df = pd.DataFrame(ka.kline)
+    df = macd(df)
+    df = df.merge(bs[['操作提示', '出现时间', '基准价格']], left_on='dt', right_on='出现时间', how='left')
+    draw(df, file_png)
+
+
+if __name__ == '__main__':
+    ts_code = '603305.SH'
+    file_bs = f"{ts_code}买卖点变化过程.xlsx"
+
+    # step 1. 仿真交易
+    # trade_simulator(ts_code, end_date='20200313', file_bs=file_bs, days=150, asset="E", watch_interval=5)
+
+    # step 2. 查看仿真交易过程的买卖点提示
+    check_trade(ts_code, '5min', file_bs)
