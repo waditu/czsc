81bd666cc23f32c8a382b592d54e72f73d51cd97
zengbin93 <zeng_bin8888@163.com>
2020-04-22 12:28:21 +0800

    0.3.1

--- chan/__init__.py
+++ chan/__init__.py
@@ -3,14 +3,8 @@
 from .analyze import KlineAnalyze
 from .ta import ma, macd, boll
 from .utils import plot_kline
-from .solid import SolidAnalyze
-from .solid import is_xd_buy, is_xd_sell
-from .solid import is_first_buy, is_first_sell
-from .solid import is_second_buy, is_second_sell
-from .solid import is_third_buy, is_third_sell
 
-
-__version__ = "0.2.21"
+__version__ = "0.3.1"
 __author__ = "zengbin93"
 __email__ = "zeng_bin8888@163.com"
 
