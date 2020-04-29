113a41c3a706aae7182fdad9fe71f7c0149daaa9
zengbin93 <zeng_bin8888@163.com>
2020-04-29 20:18:10 +0800

    0.3.1 优化

--- test/test_analyze.py
+++ test/test_analyze.py
@@ -1,7 +1,8 @@
 # coding: utf-8
 import sys
 from cobra.data.kline import get_kline
-sys.path.insert(0, r'C:\git_repo\zengbin93\chan')
+sys.path.insert(0, '.')
+sys.path.insert(0, '..')
 import chan
 from chan import KlineAnalyze
 from chan.analyze import is_bei_chi
@@ -10,14 +11,38 @@ print(chan.__version__)
 
 
 def test_bei_chi():
-    df = get_kline(ts_code="300008.SZ", end_dt="2020-03-23 15:00:00", freq='30min', asset='E')
+    df = get_kline(ts_code="000001.SH", end_dt="2020-04-28 15:00:00", freq='D', asset='I')
     ka = KlineAnalyze(df)
+
+    # 线段背驰
+    zs1 = ['2018-07-26 15:00:00', '2018-10-19 15:00:00']
+    zs2 = ['2018-01-29 15:00:00', '2018-07-06 15:00:00']
+    assert is_bei_chi(ka, zs1, zs2, direction='down', mode='xd', adjust=0.9)
+
+    zs1 = ['2013-12-10 15:00:00', '2014-01-20 15:00:00']
+    zs2 = ['2013-09-12 15:00:00', '2013-11-14 15:00:00']
+    assert not is_bei_chi(ka, zs1, zs2, direction='down', mode='xd', adjust=0.9)
+
+    # 笔背驰
+    zs1 = ['2019-05-17 15:00:00', '2019-06-10 15:00:00']
+    zs2 = ['2019-04-08 15:00:00', '2019-05-10 15:00:00']
+    assert is_bei_chi(ka, zs1, zs2, mode='bi', adjust=0.9)
+
+    zs1 = ['2018-09-28 15:00:00', '2018-10-19 15:00:00']
+    zs2 = ['2018-08-28 15:00:00', '2018-09-12 15:00:00']
+    assert not is_bei_chi(ka, zs1, zs2, mode='bi', adjust=0.9)
 
 
 def test_kline_analyze():
     df = get_kline(ts_code="300008.SZ", end_dt="2020-03-23 15:00:00", freq='30min', asset='E')
     ka = KlineAnalyze(df)
 
-    # 测试笔的识别结果
+    # 测试识别结果
+    assert ka.bi[-1]['fx_mark'] == 'g'
+    assert ka.xd[-1]['fx_mark'] == 'g'
+
+    # 测试背驰识别
+    assert not ka.bi_bei_chi()
+    assert not ka.xd_bei_chi()
 
 
