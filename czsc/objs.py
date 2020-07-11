467fd55c74671e0f6450f103729677aa2a714c75
zengbin93 <zeng_bin8888@163.com>
2020-07-11 12:27:18 +0800

    add objs

---
+++ czsc/objs.py
@@ -0,0 +1,47 @@
+# coding: utf-8
+"""
+缠论对象化
+"""
+
+
+class K:
+    """去除包含关系之后的K线"""
+    def __init__(self):
+        pass
+
+
+class FX:
+    """分型"""
+    def __repr__(self):
+        pass
+
+
+class BI:
+    """笔"""
+    def __repr__(self):
+        pass
+
+
+class XD:
+    """线段"""
+    def __repr__(self):
+        pass
+
+
+class ZS:
+    """中枢"""
+    def __repr__(self):
+        pass
+
+
+class PZ:
+    """盘整"""
+    def __repr__(self):
+        pass
+
+
+class QS:
+    """趋势"""
+    def __repr__(self):
+        pass
+
