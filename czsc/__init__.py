d61c32d65cf627b6c44b6ce75888137025886c3d
zengbin93 <zeng_bin8888@163.com>
2020-08-03 12:17:52 +0800

    0.5.2 删除最后一标记额外处理

--- czsc/__init__.py
+++ czsc/__init__.py
@@ -2,9 +2,9 @@
 
 from .ta import ma, macd, boll
 from .utils import plot_kline
-from .analyze import KlineAnalyze, is_bei_chi, find_zs
+from .analyze import KlineAnalyze, find_zs
 
-__version__ = "0.5.1"
+__version__ = "0.5.2"
 __author__ = "zengbin93"
 __email__ = "zeng_bin8888@163.com"
 
