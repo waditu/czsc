51fdb9803bec93a3d52fa18bb909fd304d5f0839
zengbin93 <zeng_bin8888@163.com>
2020-10-20 19:00:28 +0800

    0.5.8 首次提交

--- czsc/__init__.py
+++ czsc/__init__.py
@@ -3,7 +3,7 @@
 from .analyze import KlineAnalyze, find_zs
 from .ta import SMA, EMA, MACD, KDJ
 
-__version__ = "0.5.7"
+__version__ = "0.5.8"
 __author__ = "zengbin93"
 __email__ = "zeng_bin8888@163.com"
 
