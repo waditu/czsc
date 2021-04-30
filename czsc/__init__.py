180b4b87d538da0e2a19dd45c11d4cdf250ad992
zengbin93 <zeng_bin8888@163.com>
2021-04-30 21:45:39 +0800

    #47 0.7.1b update

--- czsc/__init__.py
+++ czsc/__init__.py
@@ -4,7 +4,7 @@ from .analyze import CZSC
 from .factors.factors import CzscFactors
 from .utils.ta import SMA, EMA, MACD, KDJ
 
-__version__ = "0.7.1a"
+__version__ = "0.7.1b"
 __author__ = "zengbin93"
 __email__ = "zeng_bin8888@163.com"
 
