da4a086e1cab66a288ddc6f8c26d95cb216f4b12
zengbin93 <zeng_bin8888@163.com>
2021-07-04 14:12:42 +0800

    #52 0.7.3 update

--- czsc/utils/kline_generator.py
+++ czsc/utils/kline_generator.py
@@ -1,7 +1,7 @@
 # coding: utf-8
-import warnings
 from datetime import datetime, timedelta
-from typing import List
+from typing import List, Union
+from ..enum import Freq
 from ..objects import RawBar
 
 
@@ -36,10 +36,10 @@ def bar_end_time(dt: datetime, m=1):
                 return edt
     return dt
 
-class KlineGeneratorBase:
+class KlineGenerator:
     """K线生成器，仿实盘"""
 
-    def __init__(self, max_count: int = 5000, freqs: List[str] = None):
+    def __init__(self, max_count: int = 5000, freqs: List[Union[str, Freq]] = None):
         """
 
         :param max_count: int
@@ -49,7 +49,7 @@ class KlineGeneratorBase:
         """
         self.max_count = max_count
         if freqs is None:
-            self.freqs = ['周线', '日线', '60分钟', '30分钟', '15分钟', '5分钟', '1分钟']
+            self.freqs = ['月线', '周线', '日线', '60分钟', '30分钟', '15分钟', '5分钟', '1分钟']
         else:
             self.freqs = freqs
         self.m1: List[RawBar] = []
@@ -59,6 +59,7 @@ class KlineGeneratorBase:
         self.m60: List[RawBar] = []
         self.D: List[RawBar] = []
         self.W: List[RawBar] = []
+        self.M: List[RawBar] = []
         self.end_dt = None
         self.symbol = None
 
@@ -67,205 +68,29 @@ class KlineGeneratorBase:
             self.end_dt = self.m1[-1].dt
             self.symbol = self.m1[-1].symbol
 
-    def init_kline(self, freq: str, kline: List[RawBar]):
+    def init_kline(self, freq: [Freq, str], kline: List[RawBar]):
         """输入K线进行初始化
 
         :param freq: str
         :param kline: list of dict
         :return:
         """
-        freqs_map = {"1分钟": self.m1, "5分钟": self.m5, "15分钟": self.m15,
-                     "30分钟": self.m30, "60分钟": self.m60, "日线": self.D, "周线": self.W}
-        m = freqs_map[freq]
+        freqs_map = {"1分钟": self.m1, "5分钟": self.m5, "15分钟": self.m15, "30分钟": self.m30,
+                     "60分钟": self.m60, "日线": self.D, "周线": self.W, "月线": self.M}
+        m = freqs_map[freq.value if isinstance(freq, Freq) else freq]
         m.extend(kline)
         self.__update_end_dt()
 
     def __repr__(self):
         return "<KlineGenerator for {}; latest_dt={}>".format(self.symbol, self.end_dt)
 
-    # def __update_minutes(self):
-    #     pass
-    #
-    # def __update_d(self):
-    #     pass
-    #
-    # def __update_w(self):
-    #     pass
-    #
-    # def update(self):
-    #     pass
-
-    def get_kline(self, freq: str, count: int = 1000) -> List[RawBar]:
-        """获取单个级别的K线
-
-        :param freq: str
-            级别名称，可选值 1分钟；5分钟；15分钟；30分钟；60分钟；日线；周线
-        :param count: int
-            数量
-        :return: list of dict
-        """
-        freqs_map = {"1分钟": self.m1, "5分钟": self.m5, "15分钟": self.m15,
-                     "30分钟": self.m30, "60分钟": self.m60, "日线": self.D, "周线": self.W}
-        return freqs_map[freq][-count:]
-
-    def get_klines(self, counts=None):
-        """获取多个级别的K线
-
-        :param counts: dict
-            默认值 {"1分钟": 1000, "5分钟": 1000, "30分钟": 1000, "日线": 100}
-        :return: dict of list of dict
-        """
-        if counts is None:
-            counts = {"1分钟": 1000, "5分钟": 1000, "30分钟": 1000, "日线": 100}
-        return {k: self.get_kline(k, v) for k, v in counts.items()}
-
-
-class KlineGeneratorByTick(KlineGeneratorBase):
-    """K线生成器，仿实盘，从tick开始生成"""
-
-    def __init__(self, max_count: int = 5000, freqs: List[str] = None):
-        """
-
-        :param max_count: int
-            最大K线数量
-        :param freqs: list of str
-            级别列表，默认值为 ['周线', '日线', '60分钟', '30分钟', '15分钟', '5分钟', '1分钟']
-        """
-        super().__init__(max_count, freqs)
-        warnings.warn("KlineGeneratorByTick 存在已知Bug，慎用")
-
-    def __repr__(self):
-        return "<KlineGeneratorByTick for {}; latest_dt={}>".format(self.symbol, self.end_dt)
-
-    @staticmethod
-    def __update_from_tick(last, tick):
-        new = dict(last)
-        new.update({
-            'close': round(tick['price'], 2),
-            "dt": tick['dt'],
-            "high": round(max(last['high'], tick['price']), 2),
-            "low": round(min(last['low'], tick['price']), 2),
-            "vol": last['vol'] + tick['vol']
-        })
-        if new['open'] <= 0:
-            new['open'] = round(tick['price'], 2)
-
-        if new['low'] <= 0:
-            new['low'] = round(tick['price'], 2)
-
-        return new
-
-    @staticmethod
-    def __init_bar_from_tick(tick):
-        p = round(tick['price'], 2)
-        return {
-            'symbol': tick['symbol'],
-            'open': p,
-            'close': p,
-            "dt": tick['dt'],
-            "high": p,
-            "low": p,
-            "vol": tick['vol']
-        }
-
-    def __update_minutes(self, tick=None, minutes=(1, 5, 15, 30, 60)):
-        # 更新分钟线
-        fm_map = {1: self.m1, 5: self.m5, 15: self.m15, 30: self.m30, 60: self.m60}
-
-        for minute in minutes:
-            if "{}分钟".format(minute) not in self.freqs:
-                continue
-
-            m = fm_map[minute]
-            if not m:
-                next_bar = self.__init_bar_from_tick(tick)
-                next_bar['dt'] = bar_end_time(tick['dt'], m=minute)
-                m.append(next_bar)
-            else:
-                last = m[-1]
-                next_end_dt = bar_end_time(tick['dt'], m=minute)
-                if next_end_dt > last['dt'] and next_end_dt.minute != last['dt'].minute:
-                    next_bar = self.__init_bar_from_tick(tick)
-                    next_bar['dt'] = next_end_dt
-                    m.append(next_bar)
-                else:
-                    next_bar = self.__update_from_tick(last, tick)
-                    next_bar['dt'] = next_end_dt
-                    m[-1] = next_bar
-            fm_map[minute] = m[-self.max_count:]
-
-    def __update_d(self, tick=None):
-        if "日线" not in self.freqs:
-            return
-
-        if not self.D:
-            self.D.append(self.__init_bar_from_tick(tick))
-        else:
-            last = self.D[-1]
-            if last['dt'].date() != tick['dt'].date():
-                self.D.append(self.__init_bar_from_tick(tick))
-            else:
-                self.D[-1] = self.__update_from_tick(last, tick)
-        self.D = self.D[-self.max_count:]
-
-    def __update_w(self, tick=None):
-        if "周线" not in self.freqs:
-            return
-
-        if not self.W:
-            self.W.append(self.__init_bar_from_tick(tick))
-        else:
-            last = self.W[-1]
-            if tick['dt'].weekday() == 0 and tick['dt'].weekday() != last['dt'].weekday():
-                self.W.append(self.__init_bar_from_tick(tick))
-            else:
-                self.W[-1] = self.__update_from_tick(last, tick)
-        self.W = self.W[-self.max_count:]
-
-    def update(self, tick=None):
-        """输入1分钟最新K线 或 tick，更新其他级别K线
-
-        :param tick: dict
-            {'symbol': '000001.XSHG',
-             'dt': Timestamp('2020-07-16 14:51:00'),
-             'price': 3216.8,
-             'vol': '270429600'}
-        """
-        if self.end_dt and tick['dt'] < self.end_dt:
-            warnings.warn("输入 tick 时间小于最近一个更新时间，{} < {}，不进行K线更新".format(tick['dt'], self.end_dt))
-            return
-
-        assert isinstance(tick, dict)
-        self.end_dt = tick['dt']
-        self.symbol = tick['symbol']
-        self.__update_minutes(tick, minutes=(1, 5, 15, 30, 60))
-        if "日线" in self.freqs:
-            self.__update_d(tick)
-        if "周线" in self.freqs:
-            self.__update_w(tick)
-
-
-class KlineGeneratorBy1Min(KlineGeneratorBase):
-    """K线生成器，仿实盘，从1分钟开始生成"""
-
-    def __init__(self, max_count: int = 5000, freqs: List[str] = None):
-        """
-
-        :param max_count: int
-            最大K线数量
-        :param freqs: list of str
-            级别列表，默认值为 ['周线', '日线', '60分钟', '30分钟', '15分钟', '5分钟', '1分钟']
-        """
-        super().__init__(max_count, freqs)
-
-    def __repr__(self):
-        return "<KlineGeneratorBy1Min for {}; latest_dt={}>".format(self.symbol, self.end_dt)
-
     @staticmethod
     def __update_from_1min(last: RawBar, k: RawBar, next_end_dt: datetime):
         new = RawBar(
             symbol=last.symbol,
             dt=next_end_dt,
+            id=last.id,
+            freq=last.freq,
             open=last.open,
             close=k.close,
             high=max(last.high, k.high),
@@ -275,10 +100,8 @@ class KlineGeneratorBy1Min(KlineGeneratorBase):
         return new
 
     def __update_1min(self, k: RawBar):
-        # 更新1分钟线
-        if "1分钟" not in self.freqs:
-            return
-
+        """更新1分钟线"""
+        assert '1分钟' in self.freqs
         if not self.m1:
             self.m1.append(k)
         else:
@@ -292,8 +115,9 @@ class KlineGeneratorBy1Min(KlineGeneratorBase):
         self.m1 = self.m1[-self.max_count:]
 
     def __update_minutes(self, k: RawBar, minutes=(5, 15, 30, 60)):
-        # 更新分钟线
+        """更新分钟线"""
         fm_map = {5: self.m5, 15: self.m15, 30: self.m30, 60: self.m60}
+        freq_map = {5: Freq.F5, 15: Freq.F15, 30: Freq.F30, 60: Freq.F60}
 
         for minute in minutes:
             if "{}分钟".format(minute) not in self.freqs:
@@ -302,12 +126,14 @@ class KlineGeneratorBy1Min(KlineGeneratorBase):
             m = fm_map[minute]
             next_end_dt = bar_end_time(k.dt, m=minute)
             if not m:
-                m.append(RawBar(symbol=k.symbol, dt=next_end_dt, open=k.open, close=k.close,
+                m.append(RawBar(symbol=k.symbol, id=1, freq=freq_map[minute],
+                                dt=next_end_dt, open=k.open, close=k.close,
                                 high=k.high, low=k.low, vol=k.vol))
             else:
                 last = m[-1]
                 if next_end_dt != last.dt:
-                    m.append(RawBar(symbol=k.symbol, dt=next_end_dt, open=k.open, close=k.close,
+                    m.append(RawBar(symbol=k.symbol, id=last.id+1, freq=freq_map[minute],
+                                    dt=next_end_dt, open=k.open, close=k.close,
                                     high=k.high, low=k.low, vol=k.vol))
                 else:
                     next_bar = self.__update_from_1min(last, k, next_end_dt)
@@ -318,11 +144,12 @@ class KlineGeneratorBy1Min(KlineGeneratorBase):
         if "日线" not in self.freqs:
             return
         next_end_dt = k.dt.replace(hour=0, minute=0, second=0, microsecond=0)
-        k = RawBar(symbol=k.symbol, dt=next_end_dt, open=k.open, close=k.close, high=k.high, low=k.low, vol=k.vol)
+        k = RawBar(symbol=k.symbol, id=1, freq=Freq.D, dt=next_end_dt, open=k.open, close=k.close, high=k.high, low=k.low, vol=k.vol)
         if not self.D:
             self.D.append(k)
         last = self.D[-1]
         if next_end_dt.date() != last.dt.date():
+            k.id = last.id + 1
             self.D.append(k)
         else:
             self.D[-1] = self.__update_from_1min(last, k, next_end_dt)
@@ -333,11 +160,12 @@ class KlineGeneratorBy1Min(KlineGeneratorBase):
         if "周线" not in self.freqs:
             return
         next_end_dt = k.dt.replace(hour=0, minute=0, second=0, microsecond=0)
-        k = RawBar(symbol=k.symbol, dt=next_end_dt, open=k.open, close=k.close, high=k.high, low=k.low, vol=k.vol)
+        k = RawBar(symbol=k.symbol, id=1, freq=Freq.W, dt=next_end_dt, open=k.open, close=k.close, high=k.high, low=k.low, vol=k.vol)
         if not self.W:
             self.W.append(k)
         last = self.W[-1]
         if next_end_dt.weekday() == 0 and k.dt.weekday() != last.dt.weekday():
+            k.id = last.id + 1
             self.W.append(k)
         else:
             self.W[-1] = self.__update_from_1min(last, k, next_end_dt)
@@ -345,12 +173,17 @@ class KlineGeneratorBy1Min(KlineGeneratorBase):
         self.W = self.W[-self.max_count:]
 
     def update(self, k: RawBar):
-        """输入1分钟最新K线，更新其他级别K线
+        """输入1分钟、Tick最新数据，更新其他级别K线
 
         :param k: 1分钟K线
         """
-        if self.end_dt and k.dt < self.end_dt:
-            print("输入1分钟K时间小于最近一个更新时间，{} < {}，不进行K线更新".format(k.dt, self.end_dt))
+        assert k.freq == Freq.F1, "目前仅支持从1分钟K线生成"
+        if self.m1:
+            k.id = self.m1[-1].id + 1
+        else:
+            k.id = 0
+        if self.end_dt and k.dt <= self.end_dt:
+            print("输入1分钟K时间小于最近一个更新时间，{} <= {}，不进行K线更新".format(k.dt, self.end_dt))
             return
 
         self.end_dt = k.dt
@@ -362,4 +195,28 @@ class KlineGeneratorBy1Min(KlineGeneratorBase):
             self.__update_d(k)
         if "周线" in self.freqs:
             self.__update_w(k)
+
+    def get_kline(self, freq: str, count: int = 1000) -> List[RawBar]:
+        """获取单个级别的K线
+
+        :param freq: str
+            级别名称，可选值 1分钟；5分钟；15分钟；30分钟；60分钟；日线；周线
+        :param count: int
+            数量
+        :return: list of dict
+        """
+        freqs_map = {"1分钟": self.m1, "5分钟": self.m5, "15分钟": self.m15, "30分钟": self.m30,
+                     "60分钟": self.m60, "日线": self.D, "周线": self.W, "月线": self.M}
+        return freqs_map[freq][-count:]
+
+    def get_klines(self, counts=None):
+        """获取多个级别的K线
+
+        :param counts: dict
+            默认值 {"1分钟": 1000, "5分钟": 1000, "30分钟": 1000, "日线": 100}
+        :return: dict of list of dict
+        """
+        if counts is None:
+            counts = {"1分钟": 1000, "5分钟": 1000, "30分钟": 1000, "日线": 100}
+        return {k: self.get_kline(k, v) for k, v in counts.items()}
 
