534bc89e555f5685720140204a17f498874fa08d
houwenlong <houwenlong@zto.cn>
2021-07-19 23:52:16 +0800

    完成分析第一个程序

--- czsc/extend/tester/testjqAnalysis.py
+++ czsc/extend/tester/testjqAnalysis.py
@@ -25,9 +25,21 @@ def test_data():
     )
 
     trander = JqCzscTrader(symbol="002172.XSHE", max_count=1000)
-
     print(factor_.is_match(trander.s))
     trander.open_in_browser()
     # kg = KlineGenerator(max_count=1000, freqs=['5分钟', '30分钟', '日线'])
     # trader = CzscTrader(kg=kg, get_signals=get_default_signals)
     # trader.open_in_browser()
+def test_customer():
+    factor_ = Factor(
+        name="类三买选股因子",
+        signals_any=[
+            Signal("日线_倒1笔_基础形态_类三买_任意_任意_0"),
+            Signal("日线_倒1笔_类买卖点_类三买_任意_任意_0"),
+        ],
+        signals_all=[
+            Signal("日线_倒1笔_拟合优度_小于0.2_任意_任意_0")
+        ]
+    )
+
+
