96aba268048b9ab8fb757afaed0b4ae4fa702f36
zengbin93 <zeng_bin8888@163.com>
2021-08-04 19:08:55 +0800

    0.7.4 first commit(#57)

--- czsc/__init__.py
+++ czsc/__init__.py
@@ -4,7 +4,7 @@ from .analyze import CZSC, CzscTrader
 from .utils.ta import SMA, EMA, MACD, KDJ
 from .data.jq import JqCzscTrader
 
-__version__ = "0.7.3"
+__version__ = "0.7.4"
 __author__ = "zengbin93"
 __email__ = "zeng_bin8888@163.com"
 
