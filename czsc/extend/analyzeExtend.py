48c3108c203eecba1ecddd4c8082d82915207cb1
houwenlong <houwenlong@zto.cn>
2021-08-08 23:05:15 +0800

    动态回测

--- czsc/extend/analyzeExtend.py
+++ czsc/extend/analyzeExtend.py
@@ -3,7 +3,7 @@ from czsc.objects import *
 from czsc.enum import Freq
 import pandas as pd
 from czsc.analyze import KlineGenerator
-from czsc.data.jq import get_kline
+from czsc.data.jq import get_kline, JqCzscTrader
 from czsc.data.base import freq_inv
 from czsc.signals import get_default_signals
 from typing import Union
@@ -13,6 +13,13 @@ from abc import abstractmethod
 class CZSCExtendTrader(CzscTrader):
     def __init__(self, symbol, max_count=1000, end_date=None,
                  freq_list: List[Union[str, Freq]] = ["1分钟", "5分钟", "30分钟", "日线"]):
+        """
+        使用通达信的数据生成图表信息
+        :param symbol:
+        :param max_count:
+        :param end_date:
+        :param freq_list:
+        """
         self.symbol = symbol
         if end_date:
             self.end_date = pd.to_datetime(end_date)
@@ -37,3 +44,18 @@ class CZSCExtendTrader(CzscTrader):
         for freq in kg.freqs:
             bars = get_kline(symbol, end_date=self.end_date, freq=freq_inv[freq], count=max_count)
             kg.init_kline(freq, bars)
+
+
+class JKCzscTraderExtend(JqCzscTrader):
+    """
+    使用指定的1分钟数据进行更新
+    """
+
+    def __init__(self, symbol, max_count=2000, end_date=None):
+        super(JKCzscTraderExtend, self).__init__(symbol, max_count=max_count, end_date=end_date)
+
+    def update_factors_with_bars(self, bars: List[RawBar]):
+        if not bars or bars[-1].dt <= self.end_dt:
+            return
+        for bar in bars:
+            self.check_operate(bar)
---
+++ czsc/extend/tester/BackTest.py
@@ -0,0 +1,124 @@
+from czsc.extend.utils import push_text
+from datetime import datetime
+from czsc.extend.analyzeExtend import JKCzscTraderExtend as CzscTrader
+import traceback
+import time
+import datetime
+
+import shutil
+import os
+from czsc.objects import Signal, Factor, Event, Operate
+from czsc.data.jq import get_kline
+import pandas as pd
+
+# 基础参数配置
+ct_path = os.path.join("d:\\data", "czsc_traders")
+os.makedirs(ct_path, exist_ok=True)
+symbol = '399006.XSHE'
+
+
+def start():
+    moni_path = os.path.join(ct_path, "monitor")
+    if os.path.exists(moni_path):
+        shutil.rmtree(moni_path)
+    os.makedirs(moni_path, exist_ok=True)
+    events_monitor = [
+        # 开多
+        Event(name="一买", operate=Operate.LO, factors=[
+            Factor(name="5分钟类一买", signals_all=[Signal("5分钟_倒1笔_类买卖点_类一买_任意_任意_0")]),
+            Factor(name="5分钟形一买", signals_all=[Signal("5分钟_倒1笔_基础形态_类一买_任意_任意_0")]),
+
+            Factor(name="15分钟类一买", signals_all=[Signal("15分钟_倒1笔_类买卖点_类一买_任意_任意_0")]),
+            Factor(name="15分钟形一买", signals_all=[Signal("15分钟_倒1笔_基础形态_类一买_任意_任意_0")]),
+
+            Factor(name="30分钟类一买", signals_all=[Signal("30分钟_倒1笔_类买卖点_类一买_任意_任意_0")]),
+            Factor(name="30分钟形一买", signals_all=[Signal("30分钟_倒1笔_基础形态_类一买_任意_任意_0")]),
+        ]),
+
+        Event(name="二买", operate=Operate.LO, factors=[
+            Factor(name="5分钟类二买", signals_all=[Signal("5分钟_倒1笔_类买卖点_类二买_任意_任意_0")]),
+            Factor(name="5分钟形二买", signals_all=[Signal("5分钟_倒1笔_基础形态_类二买_任意_任意_0")]),
+
+            Factor(name="15分钟类二买", signals_all=[Signal("15分钟_倒1笔_类买卖点_类二买_任意_任意_0")]),
+            Factor(name="15分钟形二买", signals_all=[Signal("15分钟_倒1笔_基础形态_类二买_任意_任意_0")]),
+
+            Factor(name="30分钟类二买", signals_all=[Signal("30分钟_倒1笔_类买卖点_类二买_任意_任意_0")]),
+            Factor(name="30分钟形二买", signals_all=[Signal("30分钟_倒1笔_基础形态_类二买_任意_任意_0")]),
+        ]),
+        Event(name="三买", operate=Operate.LO, factors=[
+            Factor(name="5分钟类三买", signals_all=[Signal("5分钟_倒1笔_类买卖点_类三买_任意_任意_0")]),
+            Factor(name="5分钟形三买", signals_all=[Signal("5分钟_倒1笔_基础形态_类三买_任意_任意_0")]),
+
+            Factor(name="15分钟类三买", signals_all=[Signal("15分钟_倒1笔_类买卖点_类三买_任意_任意_0")]),
+            Factor(name="15分钟形三买", signals_all=[Signal("15分钟_倒1笔_基础形态_类三买_任意_任意_0")]),
+
+            Factor(name="30分钟类三买", signals_all=[Signal("30分钟_倒1笔_类买卖点_类三买_任意_任意_0")]),
+            Factor(name="30分钟形三买", signals_all=[Signal("30分钟_倒1笔_基础形态_类三买_任意_任意_0")]),
+        ]),
+
+        # 平多
+        Event(name="一卖", operate=Operate.LE, factors=[
+            Factor(name="5分钟类一卖", signals_all=[Signal("5分钟_倒1笔_类买卖点_类一卖_任意_任意_0")]),
+            Factor(name="5分钟形一卖", signals_all=[Signal("5分钟_倒1笔_基础形态_类一卖_任意_任意_0")]),
+
+            Factor(name="15分钟类一卖", signals_all=[Signal("15分钟_倒1笔_类买卖点_类一卖_任意_任意_0")]),
+            Factor(name="15分钟形一卖", signals_all=[Signal("15分钟_倒1笔_基础形态_类一卖_任意_任意_0")]),
+
+            Factor(name="30分钟类一卖", signals_all=[Signal("30分钟_倒1笔_类买卖点_类一卖_任意_任意_0")]),
+            Factor(name="30分钟形一卖", signals_all=[Signal("30分钟_倒1笔_基础形态_类一卖_任意_任意_0")]),
+        ]),
+        Event(name="二卖", operate=Operate.LE, factors=[
+            Factor(name="5分钟类二卖", signals_all=[Signal("5分钟_倒1笔_类买卖点_类二卖_任意_任意_0")]),
+            Factor(name="5分钟形二卖", signals_all=[Signal("5分钟_倒1笔_基础形态_类二卖_任意_任意_0")]),
+
+            Factor(name="15分钟类二卖", signals_all=[Signal("15分钟_倒1笔_类买卖点_类二卖_任意_任意_0")]),
+            Factor(name="15分钟形二卖", signals_all=[Signal("15分钟_倒1笔_基础形态_类二卖_任意_任意_0")]),
+
+            Factor(name="30分钟类二卖", signals_all=[Signal("30分钟_倒1笔_类买卖点_类二卖_任意_任意_0")]),
+            Factor(name="30分钟形二卖", signals_all=[Signal("30分钟_倒1笔_基础形态_类二卖_任意_任意_0")]),
+        ]),
+        Event(name="三卖", operate=Operate.LE, factors=[
+            Factor(name="5分钟类三卖", signals_all=[Signal("5分钟_倒1笔_类买卖点_类三卖_任意_任意_0")]),
+            Factor(name="5分钟形三卖", signals_all=[Signal("5分钟_倒1笔_基础形态_类三卖_任意_任意_0")]),
+
+            Factor(name="15分钟类三卖", signals_all=[Signal("15分钟_倒1笔_类买卖点_类三卖_任意_任意_0")]),
+            Factor(name="15分钟形三卖", signals_all=[Signal("15分钟_倒1笔_基础形态_类三卖_任意_任意_0")]),
+
+            Factor(name="30分钟类三卖", signals_all=[Signal("30分钟_倒1笔_类买卖点_类三卖_任意_任意_0")]),
+            Factor(name="30分钟形三卖", signals_all=[Signal("30分钟_倒1笔_基础形态_类三卖_任意_任意_0")]),
+        ]),
+    ]
+    try:
+        current_date: datetime = pd.to_datetime('2021-07-28')
+        end_date = pd.to_datetime("2021-08-08")
+        ct = CzscTrader(symbol, max_count=1000, end_date=current_date)
+        data = get_kline(symbol, end_date=end_date, start_date=current_date, freq="1min")
+
+        hit_event: dict = {}
+
+        for item in data:
+            msg = f"标的代码：{symbol}\n同花顺F10：http://basic.10jqka.com.cn/{symbol.split('.')[0]}\n"
+            for event in events_monitor:
+                m, f = event.is_match(ct.s)
+                if m:
+                    print("监控提醒：date{} {}@{}\n".format(item.dt.strftime('%Y-%m-%d  %H:%M'), event.name, f))
+                    key = item.dt.strftime('%Y-%m-%d') + f
+                    contains = hit_event.get(key)
+                    if contains is None:
+                        hit_event[key] = '1'
+                        msg += "监控提醒：date{} {}@{}\n".format(item.dt.strftime('%Y-%m-%d  %H:%M'), event.name, f)
+                        hit_event
+            if "监控提醒" in msg:
+                push_text(msg.strip("\n"))
+                # 每次执行，会在moni_path下面保存一份快照
+                file_html = os.path.join(moni_path, f"{ct.symbol}_{item.dt.strftime('%Y%m%d%H%M')}.html")
+                ct.take_snapshot(file_html, width="1400px", height="580px")
+            ct.update_factors_with_bars([item])
+    except Exception as e:
+        traceback.print_exc()
+        print("{} 执行失败 - {}".format(symbol, e))
+
+
+if __name__ == '__main__':
+    start()
+    print("end")
