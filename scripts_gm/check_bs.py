c999cc92445f8d2d737ef71a668168b9d9dd93e4
zengbin93 <zeng_bin8888@163.com>
2021-08-28 19:47:34 +0800

    0.7.5 update scripts gm (#60)

--- scripts_gm/check_bs.py
+++ scripts_gm/check_bs.py
@@ -3,10 +3,17 @@
 author: zengbin93
 email: zeng_bin8888@163.com
 create_dt: 2021/6/28 22:11
+describe: 使用掘金的数据，查看任意时刻的标的状态，精确到分钟
 """
 
-from scripts_gm.gm_utils import *
+from src.utils.bt import GmCzscTrader
+from czsc.signals import get_default_signals
 
-# ct = GmCzscTrader("SZSE.002414", end_dt='2021-06-25 14:38:00+08:00')
-ct = GmCzscTrader("SZSE.002414", end_dt='2021-06-25 14:38:00+08:00')
-ct.open_in_browser()
+
+if __name__ == '__main__':
+    get_signals = get_default_signals
+
+    # ct = GmCzscTrader("SZSE.300511", end_dt='2021-07-15 14:38:00+08:00', get_signals=get_signals)
+    ct = GmCzscTrader("SZSE.300511", get_signals=get_signals)
+    ct.open_in_browser()
+
