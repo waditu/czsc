bc818105afaaa046c8b383ac05ed222ba575d60e
zengbin93 <zeng_bin8888@163.com>
2021-09-01 15:06:27 +0800

    0.7.5 fix bp(#60)

--- czsc/__init__.py
+++ czsc/__init__.py
@@ -7,7 +7,7 @@ from .data.jq import JqCzscTrader
 __version__ = "0.7.5"
 __author__ = "zengbin93"
 __email__ = "zeng_bin8888@163.com"
-__date__ = "20210830"
+__date__ = "20210831"
 
 print(f"欢迎使用CZSC！当前版本标识为 {__version__}@{__date__}")
 
