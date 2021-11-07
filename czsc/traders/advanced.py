55260208d5d5c54977b90e99160929705ef8168c
zengbin93 <zeng_bin8888@163.com>
2021-11-07 22:02:16 +0800

    0.8.2 新增支持分批开平仓的交易员对象(#65)

---
+++ czsc/traders/advanced.py
@@ -0,0 +1,126 @@
+# -*- coding: utf-8 -*-
+"""
+author: zengbin93
+email: zeng_bin8888@163.com
+create_dt: 2021/11/7 17:09
+describe: 支持分批买入卖出的高级交易员
+"""
+import os
+import webbrowser
+from typing import Callable, List
+from collections import OrderedDict
+from pyecharts.charts import Tab
+from pyecharts.components import Table
+from pyecharts.options import ComponentTitleOpts
+
+from ..analyze import CZSC
+from ..objects import PositionLong, Operate, Event, RawBar
+from ..utils import KlineGenerator
+from ..utils.cache import home_path
+
+
+class CzscAdvancedTrader:
+    """缠中说禅技术分析理论之多级别联立交易决策类（支持分批开平仓）"""
+
+    def __init__(self, kg: KlineGenerator, get_signals: Callable,
+                 long_events: List[Event] = None, long_pos: PositionLong = None):
+        """
+
+        :param kg: K线合成器
+        :param get_signals: 自定义的单级别信号计算函数
+        :param long_events: 自定义的多头交易事件组合，推荐平仓事件放到前面
+        :param long_pos: 多头仓位对象
+        """
+        self.name = "CzscAdvancedTrader"
+        self.kg = kg
+        self.freqs = kg.freqs
+        self.long_events = long_events
+        self.long_pos = long_pos
+
+        klines = self.kg.get_klines({k: 3000 for k in self.freqs})
+        self.kas = {k: CZSC(klines[k], max_bi_count=50, get_signals=get_signals) for k in klines.keys()}
+
+        self.s = self._cal_signals()
+
+    def __repr__(self):
+        return "<{} for {}>".format(self.name, self.symbol)
+
+    def take_snapshot(self, file_html=None, width="1400px", height="580px"):
+        """获取快照
+
+        :param file_html: str
+            交易快照保存的 html 文件名
+        :param width: str
+            图表宽度
+        :param height: str
+            图表高度
+        :return:
+        """
+        tab = Tab(page_title="{}@{}".format(self.symbol, self.end_dt.strftime("%Y-%m-%d %H:%M")))
+        for freq in self.freqs:
+            chart = self.kas[freq].to_echarts(width, height)
+            tab.add(chart, freq)
+
+        for freq in self.freqs:
+            t1 = Table()
+            t1.add(["名称", "数据"], [[k, v] for k, v in self.s.items() if k.startswith("{}_".format(freq))])
+            t1.set_global_opts(title_opts=ComponentTitleOpts(title="缠中说禅信号表", subtitle=""))
+            tab.add(t1, "{}信号表".format(freq))
+
+        t2 = Table()
+        ths_ = [["同花顺F10",  "http://basic.10jqka.com.cn/{}".format(self.symbol[:6])]]
+        t2.add(["名称", "数据"], [[k, v] for k, v in self.s.items() if "_" not in k] + ths_)
+        t2.set_global_opts(title_opts=ComponentTitleOpts(title="缠中说禅因子表", subtitle=""))
+        tab.add(t2, "因子表")
+
+        if file_html:
+            tab.render(file_html)
+        else:
+            return tab
+
+    def open_in_browser(self, width="1400px", height="580px"):
+        """直接在浏览器中打开分析结果"""
+        file_html = os.path.join(home_path, "temp_czsc_advanced_trader.html")
+        self.take_snapshot(file_html, width, height)
+        webbrowser.open(file_html)
+
+    def _cal_signals(self):
+        """计算信号"""
+        self.symbol = self.kas["1分钟"].symbol
+        self.end_dt = self.kas["1分钟"].bars_raw[-1].dt
+        self.latest_price = self.kas["1分钟"].bars_raw[-1].close
+
+        s = OrderedDict()
+        for freq, ks in self.kas.items():
+            s.update(ks.signals)
+
+        s.update(self.kas['1分钟'].bars_raw[-1].__dict__)
+        return s
+
+    def update(self, bar: RawBar):
+        """输入1分钟K线，更新信号，更新仓位"""
+        self.kg.update(bar)
+        klines_one = self.kg.get_klines({freq: 1 for freq in self.freqs})
+
+        for freq, klines_ in klines_one.items():
+            self.kas[freq].update(klines_[-1])
+
+        self.s = self._cal_signals()
+
+        dt = self.end_dt
+        price = self.latest_price
+        bid = self.kg.m1[-1].id
+
+        # 遍历 long_events，更新 long_pos
+        if self.long_events:
+            assert isinstance(self.long_pos, PositionLong)
+            op = Operate.HO
+            op_desc = ""
+            for event in self.long_events:
+                m, f = event.is_match(self.s)
+                if m:
+                    op = event.operate
+                    op_desc = f"{event.name}@{f}"
+            self.long_pos.update(dt, op, price, bid, op_desc)
+
+
---
+++ test/test_advanced_trader.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+author: zengbin93
+email: zeng_bin8888@163.com
+create_dt: 2021/11/7 21:07
+"""
+from tqdm import tqdm
+from czsc.traders.advanced import CzscAdvancedTrader
+from czsc.signals import get_default_signals
+from czsc.utils import KlineGenerator
+from czsc.objects import Signal, Factor, Event, Operate, PositionLong
+from test.test_analyze import read_1min
+
+def test_advanced_trader():
+    bars = read_1min()
+    kg = KlineGenerator(max_count=3000, freqs=['1分钟', '5分钟', '15分钟', '30分钟', '60分钟', '日线'])
+    for row in tqdm(bars[:150000], desc='init kg'):
+        kg.update(row)
+
+    events = [
+        Event(name="开多", operate=Operate.LO, factors=[
+            Factor(name="5分钟三买", signals_all=[Signal("5分钟_倒1笔_类买卖点_类三买_任意_任意_0")]),
+        ]),
+
+        Event(name="平多", operate=Operate.LE, factors=[
+            Factor(name="1分钟一卖", signals_all=[Signal("1分钟_倒1笔_类买卖点_类一卖_任意_任意_0")]),
+            Factor(name="5分钟一卖", signals_all=[Signal("5分钟_倒1笔_类买卖点_类一卖_任意_任意_0")]),
+            Factor(name="5分钟二卖", signals_all=[Signal("5分钟_倒1笔_类买卖点_类二卖_任意_任意_0")]),
+            Factor(name="5分钟三卖", signals_all=[Signal("5分钟_倒1笔_类买卖点_类三卖_任意_任意_0")])
+        ]),
+    ]
+    long_pos = PositionLong(symbol='000001.SH', hold_long_a=1, hold_long_b=1, hold_long_c=1, T0=True)
+    ct = CzscAdvancedTrader(kg=kg, get_signals=get_default_signals, long_events=events, long_pos=long_pos)
+    assert len(ct.s) == 215
+    for row in tqdm(bars[150000:], desc="trade"):
+        ct.update(row)
+        if long_pos.pos_changed:
+            print(" : op    : ", long_pos.operates[-1])
