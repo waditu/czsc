79567667a6b0c9ab39f0a21f840fe4632af0961a
zengbin93 <zeng_bin8888@163.com>
2022-02-18 14:14:39 +0800

    0.8.17 start coding (#65)

--- czsc/__init__.py
+++ czsc/__init__.py
@@ -6,10 +6,10 @@ from .utils.ta import SMA, EMA, MACD, KDJ
 from .objects import Signal, Factor, Event, Freq, Operate
 from . import aphorism
 
-__version__ = "0.8.16"
+__version__ = "0.8.17"
 __author__ = "zengbin93"
 __email__ = "zeng_bin8888@163.com"
-__date__ = "20220209"
+__date__ = "20220218"
 
 print(f"欢迎使用CZSC！当前版本标识为 {__version__}@{__date__}\n")
 
