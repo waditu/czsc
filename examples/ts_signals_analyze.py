20870e0494892dcd568d59018a43a44f06a361fe
zengbin93 <zeng_bin8888@163.com>
2022-03-25 14:52:44 +0800

    0.8.19 优化信号组合分析 (#65)

--- examples/ts_signals_analyze.py
+++ examples/ts_signals_analyze.py
@@ -6,6 +6,8 @@ create_dt: 2022/3/24 16:33
 describe: 使用 Tushare 数据分析信号表现
 """
 import sys
+import traceback
+
 sys.path.insert(0, '.')
 sys.path.insert(0, '..')
 
@@ -17,43 +19,63 @@ from czsc.data.ts_cache import TsDataCache
 from czsc.sensors.utils import read_cached_signals, generate_stocks_signals, analyze_signal_keys
 from czsc import signals
 
+
 pd.set_option('display.max_rows', 1000)
 pd.set_option('display.max_columns', 20)
 
 
-def get_v1_signals(c: CZSC):
-    s = OrderedDict()
-    if c.freq == Freq.D:
-        s.update(signals.bxt.get_s_three_bi(c, di=1))
-        s.update(signals.vol.get_s_vol_single_sma(c, di=1, t_seq=(10, 20)))
-    return s
+def generate_all_signals(data_path=r"C:\ts_data", name="signals_b"):
+    """给出信号定义函数，计算全市场股票的所有信号"""
+    def get_v1_signals(c: CZSC):
+        s = OrderedDict()
+        if c.freq == Freq.D:
+            s.update(signals.bxt.get_s_three_bi(c, di=1))
+            s.update(signals.vol.get_s_vol_single_sma(c, di=1, t_seq=(10, 20)))
+        return s
+
+    # tushare 研究数据缓存，一次缓存，可以重复使用
+    dc = TsDataCache(data_path, sdt='2000-01-01', edt='2022-02-18')
+
+    signals_path = os.path.join(data_path, name)
+    generate_stocks_signals(dc, signals_path, sdt='20100101', edt='20220101', base_freq='日线',
+                            freqs=['周线', '月线'], get_signals=get_v1_signals)
 
 
-data_path = r"C:\ts_data"
-dc = TsDataCache(data_path, sdt='2000-01-01', edt='2022-02-18')
-signals_path = os.path.join(data_path, 'signals_b')
+def analyze_signals():
+    results_path = r"C:\ts_data\signals_b_analyze"
+    signals_path = r"C:\ts_data\signals_b"
+    path_pat = f"{signals_path}\*_signals.pkl"
+    sdt = "20150101"
+    edt = "20220101"
+    file_output = os.path.join(signals_path, f"{sdt}_{edt}_merged.pkl")
+    dfs = read_cached_signals(file_output, path_pat, sdt, edt)
 
-# if not os.path.exists(signals_path):
-#     generate_stocks_signals(dc, signals_path, sdt='20100101', edt='20220101', base_freq='日线',
-#                             freqs=['周线', '月线'], get_signals=get_v1_signals)
-
-# dfs = read_cached_signals(os.path.join(data_path, "signals_b_20180101_20220101.pkl"),
-#                           path_pat=f"{signals_path}\*_signals.pkl", sdt="2018-01-01", edt="2022-01-01")
-
-dfs = read_cached_signals(os.path.join(data_path, "signals_b_20180101_20220101.pkl"),
-                          path_pat=rf"C:\ts_data\signals_v2\*.pkl", sdt="2018-01-01", edt="2022-01-01")
-
-
-def report_signal_performance():
-    dfr = analyze_signal_keys(dfs, keys=['日线_倒1K1B均额_1至4千万'], mode=2)
-
-    # 按年分析
+    # 为了方便逐年查看信号表现，新增 year
     dfs['year'] = dfs['dt'].apply(lambda x: x.year)
 
-    results = []
-    keys = ['日线_倒1K1B均额_1至4千万']
-    for year, df_ in dfs.groupby('year'):
-        dfr_ = analyze_signal_keys(df_, keys, mode=2)
-        dfr_['year'] = year
-        results.append(dfr_)
-    dfr = pd.concat(results, ignore_index=True)
+    os.makedirs(results_path, exist_ok=True)
+    for col in [x for x in dfs.columns if len(x.split("_")) == 3]:
+        try:
+            # 整体结果
+            dfr = analyze_signal_keys(dfs, keys=[col], mode=2)
+            dfr['year'] = 0
+
+            # 按年分析
+            results = [dfr]
+            for year, df_ in dfs.groupby('year'):
+                dfr_ = analyze_signal_keys(df_, keys=[col], mode=2)
+                dfr_['year'] = year
+                results.append(dfr_)
+            dfr = pd.concat(results, ignore_index=True)
+
+            file_res = os.path.join(results_path, f"{col}_{sdt}_{edt}.xlsx")
+            dfr.to_excel(file_res, index=False)
+            print(f"signal results saved into {file_res}")
+        except:
+            print(f"signal analyze failed: {col}")
+            traceback.print_exc()
+
+
+if __name__ == '__main__':
+    analyze_signals()
+
