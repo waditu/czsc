699b70824262c20a03cc81550e782f34973f633b
zengbin93 <zeng_bin8888@163.com>
2022-04-04 19:09:34 +0800

    0.8.22 start coding (#65)

--- czsc/__init__.py
+++ czsc/__init__.py
@@ -15,10 +15,10 @@ from .traders.advanced import CzscAdvancedTrader
 from .utils.ta import SMA, EMA, MACD, KDJ
 from .objects import Freq, Operate, Direction, Signal, Factor, Event, RawBar, NewBar
 
-__version__ = "0.8.21"
+__version__ = "0.8.22"
 __author__ = "zengbin93"
 __email__ = "zeng_bin8888@163.com"
-__date__ = "20220402"
+__date__ = "20220404"
 
 print(f"欢迎使用CZSC！当前版本标识为 {__version__}@{__date__}\n")
 
