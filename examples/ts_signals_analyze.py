61f3cf68dca12f8b2870206d36fcf6cc69d6a86d
zengbin93 <zeng_bin8888@163.com>
2022-05-11 21:08:38 +0800

    0.8.26 修复tushare数据的信号生成 (#65)

--- examples/ts_signals_analyze.py
+++ examples/ts_signals_analyze.py
@@ -8,7 +8,7 @@ describe: 使用 Tushare 数据分析信号表现
 import os
 import traceback
 import pandas as pd
-from czsc import CZSC, Freq
+from czsc import CZSC, Freq, CzscAdvancedTrader
 from collections import OrderedDict
 from czsc.data.ts_cache import TsDataCache
 from czsc.sensors.utils import read_cached_signals, generate_stocks_signals, SignalsPerformance
@@ -21,19 +21,27 @@ pd.set_option('display.max_columns', 20)
 
 def generate_all_signals(data_path=r"C:\ts_data", name="signals_b"):
     """给出信号定义函数，计算全市场股票的所有信号"""
-    def get_v1_signals(c: CZSC):
+    def get_v1_signals(cat: CzscAdvancedTrader):
         s = OrderedDict()
-        if c.freq == Freq.D:
-            s.update(signals.bxt.get_s_three_bi(c, di=1))
-            s.update(signals.vol.get_s_vol_single_sma(c, di=1, t_seq=(10, 20)))
+        for freq, c in cat.kas.items():
+            if c.freq == Freq.D:
+                s.update(signals.bxt.get_s_three_bi(c, di=1))
+                s.update(signals.vol.get_s_vol_single_sma(c, di=1, t_seq=(10, 20)))
         return s
+
+    def __strategy(symbol):
+        return {
+            "symbol": symbol,
+            "get_signals": get_v1_signals,
+            "base_freq": '日线',
+            "freqs": ['周线', '月线'],
+        }
 
     # tushare 研究数据缓存，一次缓存，可以重复使用
     dc = TsDataCache(data_path, sdt='2000-01-01', edt='2022-02-18')
 
     signals_path = os.path.join(data_path, name)
-    generate_stocks_signals(dc, signals_path, sdt='20100101', edt='20220101', base_freq='日线',
-                            freqs=['周线', '月线'], get_signals=get_v1_signals)
+    generate_stocks_signals(dc, signals_path, sdt='20100101', edt='20220101', strategy=__strategy)
 
 
 def analyze_signals():
@@ -61,5 +69,6 @@ def analyze_signals():
 
 
 if __name__ == '__main__':
+    generate_all_signals(data_path=r"C:\ts_data_czsc", name="signals_b")
     analyze_signals()
 
