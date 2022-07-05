623951133e87f612c24663a49a0a3240a1a4d63e
zengbin93 <zeng_bin8888@163.com>
2022-07-05 10:42:42 +0800

    0.8.29 start coding (#65)

--- czsc/__init__.py
+++ czsc/__init__.py
@@ -16,10 +16,10 @@ from .utils.ta import SMA, EMA, MACD, KDJ
 from .objects import Freq, Operate, Direction, Signal, Factor, Event, RawBar, NewBar
 from .utils.cache import home_path, get_dir_size, empty_cache_path
 
-__version__ = "0.8.28"
+__version__ = "0.8.29"
 __author__ = "zengbin93"
 __email__ = "zeng_bin8888@163.com"
-__date__ = "20220606"
+__date__ = "20220705"
 
 print(f"CZSC环境变量："
       f"czsc_min_bi_len = {envs.get_min_bi_len()}; "
@@ -32,4 +32,4 @@ if envs.get_welcome():
 
 
 if get_dir_size(home_path) > pow(1024, 3) and envs.get_verbose():
-    print(f"{home_path} 目录缓存超过1GB，请适当清理。调用 empty_cache_path 可以直接清空缓存")
+    print(f"{home_path} 目录缓存超过1GB，请适当清理。调用 czsc.empty_cache_path 可以直接清空缓存")
