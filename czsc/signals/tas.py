483303e15cccfff050a7cef2c3f987852b8ec99c
zengbin93 <zeng_bin8888@163.com>
2022-11-19 23:52:33 +0800

    0.9.2 新增MACD强弱信号

--- czsc/signals/tas.py
+++ czsc/signals/tas.py
@@ -187,6 +187,49 @@ def tas_macd_direct_V221106(c: CZSC, di: int = 1) -> OrderedDict:
     return s
 
 
+def tas_macd_power_V221108(c: CZSC, di: int = 1) -> OrderedDict:
+    """MACD强弱
+
+    **信号逻辑：**
+
+    1. 指标超强满足条件：DIF＞DEA＞0；释义：指标超强表示市场价格处于中长期多头趋势中，可能形成凌厉的逼空行情
+    2. 指标强势满足条件：DIF-DEA＞0（MACD柱线＞0）释义：指标强势表示市场价格处于中短期多头趋势中，价格涨多跌少，通常是反弹行情
+    3. 指标弱势满足条件：DIF-DEA＜0（MACD柱线＜0）释义：指标弱势表示市场价格处于中短期空头趋势中，价格跌多涨少，通常是回调行情
+    4. 指标超弱满足条件：DIF＜DEA＜0释义：指标超弱表示市场价格处于中长期空头趋势中，可能形成杀多行情
+
+    **信号列表：**
+
+    - Signal('60分钟_D1K_MACD强弱_超强_任意_任意_0')
+    - Signal('60分钟_D1K_MACD强弱_弱势_任意_任意_0')
+    - Signal('60分钟_D1K_MACD强弱_超弱_任意_任意_0')
+    - Signal('60分钟_D1K_MACD强弱_强势_任意_任意_0')
+
+    :param c: CZSC对象
+    :param di: 信号产生在倒数第di根K线
+    :return: 信号识别结果
+    """
+    k1, k2, k3 = f"{c.freq.value}_D{di}K_MACD强弱".split("_")
+
+    v1 = "其他"
+    if len(c.bars_raw) > di + 10:
+        bar = c.bars_raw[-di]
+        dif, dea = bar.cache['MACD']['dif'], bar.cache['MACD']['dea']
+
+        if dif >= dea >= 0:
+            v1 = "超强"
+        elif dif - dea > 0:
+            v1 = "强势"
+        elif dif <= dea <= 0:
+            v1 = "超弱"
+        elif dif - dea < 0:
+            v1 = "弱势"
+
+    s = OrderedDict()
+    signal = Signal(k1=k1, k2=k2, k3=k3, v1=v1)
+    s[signal.key] = signal.value
+    return s
+
+
 def tas_macd_change_V221105(c: CZSC, di: int = 1, n: int = 55) -> OrderedDict:
     """MACD颜色变化；贡献者：马鸣
 
