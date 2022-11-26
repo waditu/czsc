8d5c7bd24e014812fc19a40e73f6e8a63230dc99
zengbin93 <zeng_bin8888@163.com>
2022-11-26 21:30:15 +0800

    0.9.3 新增一个简洁、粗糙的一买信号识别

--- examples/strategies/check_signal.py
+++ examples/strategies/check_signal.py
@@ -14,73 +14,81 @@ from loguru import logger
 from czsc import CZSC, signals, RawBar, Direction
 from czsc.data import TsDataCache, get_symbols
 from czsc.utils import get_sub_elements, single_linear
-from czsc.objects import Freq, Operate, Signal, Factor, Event
+from czsc.objects import Freq, Operate, Signal, Factor, Event, BI
 from czsc.traders import CzscAdvancedTrader
 
 
 # 定义信号函数
 # ----------------------------------------------------------------------------------------------------------------------
 
-def jcc_ta_xing_221124(c: CZSC, di: int = 1) -> OrderedDict:
-    """塔形顶底
 
-    **信号逻辑：**
-
-    1. 首尾两根K线的实体最大
-    2. 首k上涨，尾K下跌，且中间高点相近，且低点大于首尾低点的较大者，塔形顶部；反之，底部。
+def cxt_first_buy_V221126(c: CZSC, di=1) -> OrderedDict:
+    """单级别一买信号
 
     **信号列表：**
 
-    - Signal('15分钟_D1K_塔形_顶部_6K_任意_0')
-    - Signal('15分钟_D1K_塔形_顶部_9K_任意_0')
-    - Signal('15分钟_D1K_塔形_底部_7K_任意_0')
-    - Signal('15分钟_D1K_塔形_顶部_5K_任意_0')
-    - Signal('15分钟_D1K_塔形_底部_5K_任意_0')
-    - Signal('15分钟_D1K_塔形_底部_8K_任意_0')
-    - Signal('15分钟_D1K_塔形_底部_6K_任意_0')
-    - Signal('15分钟_D1K_塔形_顶部_7K_任意_0')
-    - Signal('15分钟_D1K_塔形_顶部_8K_任意_0')
-    - Signal('15分钟_D1K_塔形_底部_9K_任意_0')
+    - Signal('15分钟_D1B_BUY1_一买_5笔_任意_0')
+    - Signal('15分钟_D1B_BUY1_一买_11笔_任意_0')
+    - Signal('15分钟_D1B_BUY1_一买_7笔_任意_0')
+    - Signal('15分钟_D1B_BUY1_一买_21笔_任意_0')
+    - Signal('15分钟_D1B_BUY1_一买_17笔_任意_0')
+    - Signal('15分钟_D1B_BUY1_一买_19笔_任意_0')
+    - Signal('15分钟_D1B_BUY1_一买_9笔_任意_0')
+    - Signal('15分钟_D1B_BUY1_一买_15笔_任意_0')
+    - Signal('15分钟_D1B_BUY1_一买_13笔_任意_0')
 
     :param c: CZSC 对象
-    :param di: 倒数第di跟K线
-    :return: 识别结果
+    :param di: CZSC 对象
+    :return: 信号字典
     """
-    def __check_ta_xing(bars: List[RawBar]):
-        if len(bars) < 5:
-            return "其他"
 
-        rb, lb = bars[0], bars[-1]
-        sorted_solid = sorted([x.solid for x in bars])
-        if min(rb.solid, lb.solid) >= sorted_solid[-2]:
+    def __check_first_buy(bis: List[BI]):
+        """检查 bis 是否是一买的结束
 
-            g_c1 = rb.close > rb.open and lb.close < lb.open
-            g_c2 = np.var([x.high for x in bars[1: -1]]) < 0.5
-            g_c3 = all(x.low > max(rb.open, lb.close) for x in bars[1: -1])
-            if g_c1 and g_c2 and g_c3:
-                return "顶部"
+        :param bis: 笔序列，按时间升序
+        """
+        res = {"match": False, "v1": "一买", "v2": f"{len(bis)}笔", 'v3': "任意"}
+        if len(bis) % 2 != 1 or bis[-1].direction == Direction.Up or bis[0].direction != bis[-1].direction:
+            return res
 
-            d_c1 = rb.close < rb.open and lb.close > lb.open
-            d_c2 = np.var([x.low for x in bars[1: -1]]) < 0.5
-            d_c3 = all(x.high < min(rb.open, lb.close) for x in bars[1: -1])
-            if d_c1 and d_c2 and d_c3:
-                return "底部"
+        if max([x.high for x in bis]) != bis[0].high or min([x.low for x in bis]) != bis[-1].low:
+            return res
 
-        return "其他"
+        # 检查背驰：获取向下突破的笔列表
+        key_bis = []
+        for i in range(0, len(bis) - 2, 2):
+            if i == 0:
+                key_bis.append(bis[i])
+            else:
+                b1, _, b3 = bis[i - 2:i + 1]
+                if b3.low < b1.low:
+                    key_bis.append(b3)
 
-    k1, k2, k3 = f"{c.freq.value}_D{di}K_塔形".split("_")
+        # 检查背驰：最后一笔的 power_price，power_volume，length 同时满足背驰条件才算背驰
+        bc_price = bis[-1].power_price < max(bis[-3].power_price, np.mean([x.power_price for x in key_bis]))
+        bc_volume = bis[-1].power_volume < max(bis[-3].power_volume, np.mean([x.power_volume for x in key_bis]))
+        bc_length = bis[-1].length < max(bis[-3].length, np.mean([x.length for x in key_bis]))
 
-    for n in (5, 6, 7, 8, 9):
-        _bars = get_sub_elements(c.bars_raw, di=di, n=n)
-        v1 = __check_ta_xing(_bars)
-        if v1 != "其他":
-            v2 = f"{n}K"
+        if bc_price and (bc_volume or bc_length):
+            res['match'] = True
+        return res
+
+    k1, k2, k3 = c.freq.value, f"D{di}B", "BUY1"
+    v1, v2, v3 = "其他", '任意', '任意'
+
+    for n in (21, 19, 17, 15, 13, 11, 9, 7, 5):
+        _bis = get_sub_elements(c.bi_list, di=di, n=n)
+        if len(_bis) != n:
+            logger.warning('笔的数量不对')
+            continue
+
+        _res = __check_first_buy(_bis)
+        if _res['match']:
+            v1, v2, v3 = _res['v1'], _res['v2'], _res['v3']
             break
-        else:
-            v2 = "其他"
 
     s = OrderedDict()
-    signal = Signal(k1=k1, k2=k2, k3=k3, v1=v1, v2=v2)
+    signal = Signal(k1=k1, k2=k2, k3=k3, v1=v1, v2=v2, v3=v3)
     s[signal.key] = signal.value
     return s
 
@@ -94,7 +102,7 @@ def trader_strategy(symbol):
         s = OrderedDict({"symbol": cat.symbol, "dt": cat.end_dt, "close": cat.latest_price})
         # signals.update_macd_cache(cat.kas['60分钟'])
         # logger.info('\n\n')
-        s.update(jcc_ta_xing_221124(cat.kas['15分钟'], di=1))
+        s.update(cxt_first_buy_V221126(cat.kas['15分钟'], di=1))
         return s
 
     tactic = {
