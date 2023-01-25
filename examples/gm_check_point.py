1db99a10d5509524cac05bcc716d0cc99f0672e2
zengbin93 <zeng_bin8888@163.com>
2023-01-25 16:25:04 +0800

    0.9.7 update

--- examples/gm_check_point.py
+++ examples/gm_check_point.py
@@ -9,14 +9,12 @@ from czsc.gms.gm_stocks import strategy_snapshot, gm_take_snapshot
 from czsc.strategies import trader_strategy_a as strategy
 
 if __name__ == '__main__':
-    _symbol = "SHSE.000016"
-    # 查看含策略交易信号的快照
-    ct = strategy_snapshot(_symbol, end_dt="2022-07-27 13:15", strategy=strategy)
+    # _symbol = "SHSE.000016"
+    # # 查看含策略交易信号的快照
+    # ct = strategy_snapshot(_symbol, end_dt="2022-07-27 13:15", strategy=strategy)
+    #
+    # # 仅查看分型、笔的程序化识别
+    # cts = gm_take_snapshot(_symbol, end_dt="2022-07-27 13:15", max_count=1000)
 
-    # 仅查看分型、笔的程序化识别
-    cts = gm_take_snapshot(_symbol, end_dt="2022-07-27 13:15", max_count=1000)
-    cts.open_in_browser()
-
-
-
+    cts = gm_take_snapshot("SZSE.002536", end_dt="2022-07-27 13:15", max_count=1000)
 
