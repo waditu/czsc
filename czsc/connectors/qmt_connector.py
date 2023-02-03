09199899c7a090b91ff42917b101e4bbb9cf8902
zengbin93 <zeng_bin8888@163.com>
2023-02-03 21:15:09 +0800

    0.9.7 update

--- czsc/connectors/qmt_connector.py
+++ czsc/connectors/qmt_connector.py
@@ -7,9 +7,12 @@ describe:
 """
 import pandas as pd
 from typing import List
+from loguru import logger
 from deprecated import deprecated
 from czsc.objects import Freq, RawBar
+from czsc.fsa.im import IM
 from xtquant import xtdata
+from xtquant.xttrader import XtQuantTraderCallback
 
 
 def format_stock_kline(kline: pd.DataFrame, freq: Freq) -> List[RawBar]:
@@ -154,6 +157,86 @@ def get_symbols(step):
                  '159981.SZ', '159949.SZ', '159915.SZ'],
     }
     return stocks_map[step]
+
+
+class TraderCallback(XtQuantTraderCallback):
+    """基础回调类，主要是一些日志和IM通知功能"""
+    def __init__(self, **kwargs):
+        super(TraderCallback, self).__init__()
+        self.kwargs = kwargs
+
+        if kwargs.get('feishu_app_id', None) and kwargs.get('feishu_app_secret', None):
+            self.im = IM(app_id=kwargs['feishu_app_id'], app_secret=kwargs['feishu_app_secret'])
+            self.members = kwargs['feishu_members']
+        else:
+            self.im = None
+            self.members = None
+
+        file_log = kwargs.get('file_log', None)
+        if file_log:
+            logger.add(file_log, rotation='1 day', encoding='utf-8', enqueue=True)
+        self.file_log = file_log
+        logger.info(f"TraderCallback init: {kwargs}")
+
+    def on_disconnected(self):
+        """连接断开"""
+        logger.info("connection lost")
+
+    def on_stock_order(self, order):
+        """委托回报推送
+
+        :param order: XtOrder对象
+        """
+        logger.info(f"on order callback: {order.stock_code} {order.order_status} {order.order_sysid}")
+
+    def on_stock_asset(self, asset):
+        """资金变动推送
+
+        :param asset: XtAsset对象
+        """
+        logger.info(f"on asset callback: {asset.account_id} {asset.cash} {asset.total_asset}")
+
+    def on_stock_trade(self, trade):
+        """成交变动推送
+
+        :param trade: XtTrade对象
+        """
+        logger.info(f"on trade callback: {trade.account_id} {trade.stock_code} {trade.order_id}")
+
+    def on_stock_position(self, position):
+        """持仓变动推送
+
+        :param position: XtPosition对象
+        """
+        logger.info(f"on position callback: {position.stock_code} {position.volume}")
+
+    def on_order_error(self, order_error):
+        """委托失败推送
+
+        :param order_error:XtOrderError 对象
+        """
+        logger.info(f"on order_error callback: {order_error.order_id} {order_error.error_id} {order_error.error_msg}")
+
+    def on_cancel_error(self, cancel_error):
+        """撤单失败推送
+
+        :param cancel_error: XtCancelError 对象
+        """
+        logger.info(f"{cancel_error.order_id} {cancel_error.error_id} {cancel_error.error_msg}")
+
+    def on_order_stock_async_response(self, response):
+        """异步下单回报推送
+
+        :param response: XtOrderResponse 对象
+        """
+        logger.info(f"on_order_stock_async_response: {response.order_id} {response.seq}")
+
+    def on_account_status(self, status):
+        """账户状态变化推送
+
+        :param status: XtAccountStatus 对象
+        """
+        logger.info(f"on_account_status: {status.account_id} {status.account_type} {status.status}")
 
 
 def test_get_kline():
