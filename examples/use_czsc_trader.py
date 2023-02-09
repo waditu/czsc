8422cc8d5f32d1385c9eb289c8e304480a97d50d
zengbin93 <zeng_bin8888@163.com>
2023-02-09 15:54:55 +0800

    0.9.7 update examples

--- examples/use_czsc_trader.py
+++ examples/use_czsc_trader.py
@@ -24,7 +24,7 @@ def use_czsc_trader_by_tushare():
     bars = dc.pro_bar_minutes(ts_code=symbol, asset='E', freq='5min',
                               sdt='20151101', edt='20210101', adj='hfq', raw_bar=True)
 
-    tactic = CzscStrategySMA5(symbol="000001.SHSE")
+    tactic = CzscStrategySMA5(symbol=symbol)
     trader = tactic.init_trader(bars, sdt='20200801')
     # trader = tactic.replay(bars, res_path=r"C:\ts_data_czsc\trade_replay_test_c", sdt='20170101')
     print(trader.positions[0].evaluate_pairs())
@@ -36,9 +36,12 @@ def use_czsc_trader_by_qmt():
     symbol = '000001.SZ'
     bars = qmc.get_kline(symbol, period='5m', start_time='20151101', end_time='20210101',
                          dividend_type='back', download_hist=True, df=False)
-    tactic = CzscStrategySMA5(symbol="000001.SHSE", freqs=['5分钟', '15分钟', '30分钟', '日线'])
-    trader = tactic.init_trader(bars, sdt='20200801')
-    # trader = tactic.replay(bars, res_path=r"C:\ts_data_czsc\trade_replay_test_c", sdt='20170101')
+    tactic = CzscStrategySMA5(symbol=symbol)
+    # 初始化交易对象
+    # trader = tactic.init_trader(bars, sdt='20200801')
+
+    # 执行策略回放，生成交易快照文件
+    trader = tactic.replay(bars, res_path=r"C:\ts_data_czsc\trade_replay_test_c", sdt='20170101')
     print(trader.positions[0].evaluate_pairs())
 
 
