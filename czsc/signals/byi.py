30e21a00020e094a35d70cabed8b8deb7a59a3f0
zengbin93 <zeng_bin8888@163.com>
2023-03-17 14:09:05 +0800

    0.9.13 fix bug

--- czsc/signals/byi.py
+++ czsc/signals/byi.py
@@ -11,7 +11,7 @@ from collections import OrderedDict
 from typing import List
 from czsc import CZSC
 from czsc.objects import Signal, BI, Direction
-from czsc.utils import get_sub_elements
+from czsc.utils import get_sub_elements, create_single_signal
 from czsc.utils.sig import is_symmetry_zs
 
 
@@ -24,7 +24,7 @@ def byi_symmetry_zs_V221107(c: CZSC, di=1):
     准差小于均值的一定比例，则认为是对称中枢
 
     **信号列表：**
-    
+
     - Signal('15分钟_D1B_对称中枢_否_向下_任意_0')
     - Signal('15分钟_D1B_对称中枢_是_向上_7笔_0')
     - Signal('15分钟_D1B_对称中枢_否_向上_任意_0')
@@ -40,6 +40,10 @@ def byi_symmetry_zs_V221107(c: CZSC, di=1):
     """
     bis: List[BI] = get_sub_elements(c.bi_list, di=di, n=10)
     k1, k2, k3 = f"{c.freq.value}_D{di}B_对称中枢".split("_")
+    v1 = '其他'
+    if len(bis) < 7:
+        return create_single_signal(k1=k1, k2=k2, k3=k3, v1=v1)
+
     for i in (7, 5, 3):
         v1 = is_symmetry_zs(bis[-i:])
         if v1:
@@ -50,11 +54,5 @@ def byi_symmetry_zs_V221107(c: CZSC, di=1):
 
     v1 = "是" if v1 else "否"
     v2 = "向上" if bis[-1].direction == Direction.Down else "向下"
-
-    s = OrderedDict()
-    x1 = Signal(k1=k1, k2=k2, k3=k3, v1=v1, v2=v2, v3=v3)
-    s[x1.key] = x1.value
-    return s
-
-
+    return create_single_signal(k1=k1, k2=k2, k3=k3, v1=v1, v2=v2, v3=v3)
 
