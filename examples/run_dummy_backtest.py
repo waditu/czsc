e7299652a508c4269718a70094e78ec152652896
zengbin93 <zeng_bin8888@163.com>
2023-04-01 15:42:22 +0800

    0.9.15 update

--- examples/run_dummy_backtest.py
+++ examples/run_dummy_backtest.py
@@ -10,23 +10,26 @@ sys.path.insert(0, '.')
 sys.path.insert(0, '..')
 
 import czsc
-from czsc.connectors import qmt_connector as qmc
-from czsc.strategies import CzscStocksBeta
+from czsc.connectors.research import get_raw_bars, get_symbols
+from czsc.strategies import CzscStrategyExample2
 
 
-dummy = czsc.DummyBacktest(strategy=CzscStocksBeta, read_bars=qmc.get_raw_bars, sdt='20200101', edt='20230301',
+dummy = czsc.DummyBacktest(strategy=CzscStrategyExample2, read_bars=get_raw_bars,
+                           signals_module_name='czsc.signals',
+                           sdt='20200101', edt='20230301',
                            signals_path=r'D:\QMT投研\CzscStocksBeta\signals',
                            results_path=r'D:\QMT投研\CzscStocksBeta\results_20200101_20230301')
 
+# 定义需要回测的品种，这里可以自定义
+symbols = get_symbols('A股主要指数')
+
 # # 查看某个品种的交易回放
-# dummy.replay('000001.SZ')
+# dummy.replay(symbols[0)
 
 
 # Python中的多进程必须在if __name__ == '__main__'中执行，否则会报错
 
 if __name__ == '__main__':
-    # 定义需要回测的品种，这里可以自定义
-    symbols = qmc.get_symbols('train')
     # 这里仅回测前10个品种，作为执行示例
     dummy.execute(symbols[:10], n_jobs=4)
 
