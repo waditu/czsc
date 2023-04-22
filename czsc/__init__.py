7ea420c4baa37c38e4068a347d3749e247166a6d
zengbin93 <zeng_bin8888@163.com>
2023-04-22 14:05:22 +0800

    0.9.18 新增Json配置策略

--- czsc/__init__.py
+++ czsc/__init__.py
@@ -17,7 +17,7 @@ from czsc.utils.cache import home_path, get_dir_size, empty_cache_path
 from czsc.traders import CzscTrader, CzscSignals, generate_czsc_signals, check_signals_acc, get_unique_signals
 from czsc.traders import PairsPerformance, combine_holds_and_pairs, combine_dates_and_pairs, stock_holds_performance
 from czsc.traders import DummyBacktest, SignalsParser, get_signals_by_conf, get_signals_config, get_signals_freqs
-from czsc.strategies import CzscStrategyBase
+from czsc.strategies import CzscStrategyBase, CzscJsonStrategy
 from czsc.utils import KlineChart, BarGenerator, resample_bars, dill_dump, dill_load, read_json, save_json
 from czsc.utils import get_sub_elements, get_py_namespace, freqs_sorted, x_round, import_by_name, create_grid_params
 from czsc.utils import cal_trade_price, cross_sectional_ic, update_bbars, update_tbars, update_nbars
