bf13e4aa3c444ae6501f73bb9e0db3b4d390d8e9
zengbin93 <zeng_bin8888@163.com>
2023-05-11 20:52:10 +0800

    0.9.19 新增信号函数

--- czsc/signals/pos.py
+++ czsc/signals/pos.py
@@ -9,6 +9,68 @@ from collections import OrderedDict
 from czsc.traders.base import CzscTrader
 from czsc.utils import create_single_signal
 from czsc.objects import Operate, Direction, Mark
+from czsc.signals.tas import update_ma_cache
+
+
+def pos_ma_V230414(cat: CzscTrader, **kwargs) -> OrderedDict:
+    """判断开仓后是否升破MA均线或跌破MA均线
+
+    参数模板："{pos_name}_{freq1}#{ma_type}#{timeperiod}_持有状态V230414"
+
+    **信号逻辑：**
+
+    多头止损逻辑如下，反之为空头止损逻辑：
+
+    1. 从多头开仓点开始，在给定对的K线周期 freq1 上向前找 N 个底分型，记为 F1
+    2. 将这 N 个底分型的最低点，记为 L1，如果 L1 的价格低于开仓点的价格，则止损
+
+    **信号列表：**
+
+    - Signal('日线三买多头N1_60分钟#SMA#5_持有状态V230414_多头_升破均线_任意_0')
+    - Signal('日线三买多头N1_60分钟#SMA#5_持有状态V230414_空头_跌破均线_任意_0')
+
+    :param cat: CzscTrader对象
+    :param kwargs: 参数字典
+        - pos_name: str，开仓信号的名称
+        - freq1: str，给定的K线周期
+        - n: int，向前找的分型个数，默认为 3
+    :return:
+    """
+    pos_name = kwargs["pos_name"]
+    freq1 = kwargs["freq1"]
+    ma_type = kwargs.get("ma_type", "SMA").upper()
+    timeperiod = int(kwargs.get("timeperiod", 5))
+    k1, k2, k3 = f"{pos_name}_{freq1}#{ma_type}#{timeperiod}_持有状态V230414".split("_")
+    v1, v2 = '其他', '其他'
+    key = update_ma_cache(cat.kas[freq1], ma_type=ma_type, timeperiod=timeperiod)
+    # 如果没有持仓策略，则不产生信号
+    if not hasattr(cat, "positions"):
+        return create_single_signal(k1=k1, k2=k2, k3=k3, v1=v1)
+
+    pos = [x for x in cat.positions if x.name == pos_name][0]
+    if len(pos.operates) == 0 or pos.operates[-1]['op'] in [Operate.SE, Operate.LE]:
+        return create_single_signal(k1=k1, k2=k2, k3=k3, v1=v1)
+
+    c = cat.kas[freq1]
+    op = pos.operates[-1]
+
+    # 多头止损逻辑
+    if op['op'] == Operate.LO:
+        bars = [x for x in c.bars_raw[-100:] if x.dt > op['dt']]
+        for x in bars:
+            if x.close > x.cache[key]:
+                v1, v2 = '多头', '升破均线'
+                break
+
+    # 空头止损逻辑
+    if op['op'] == Operate.SO:
+        bars = [x for x in c.bars_raw[-100:] if x.dt > op['dt']]
+        for x in bars:
+            if x.close < x.cache[key]:
+                v1, v2 = '空头', '跌破均线'
+                break
+
+    return create_single_signal(k1=k1, k2=k2, k3=k3, v1=v1, v2=v2)
 
 
 def pos_fx_stop_V230414(cat: CzscTrader, **kwargs) -> OrderedDict:
