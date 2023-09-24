e2f305d50d23e3a42fa6b837bc5ecdf194c70de1
zengbin93 <zeng_bin8888@163.com>
2023-09-24 21:59:54 +0800

    0.9.29 新增 cxt_bi_trend_V230913 信号函数

--- examples/signals_dev/signal_match.py
+++ examples/signals_dev/signal_match.py
@@ -3,7 +3,7 @@
 author: zengbin93
 email: zeng_bin8888@163.com
 create_dt: 2023/3/30 12:08
-describe: 
+describe:
 """
 import os
 import sys
@@ -44,8 +44,8 @@ if __name__ == '__main__':
     conf = sp.parse(signals_seq)
     parsed_name = {x['name'] for x in conf}
     print(f"total signal functions: {len(sp.sig_name_map)}; parsed: {len(parsed_name)}")
-    # total signal functions: 182; parsed: 182
-    
+    # total signal functions: 197; parsed: 197
+
     # 测试信号配置生成信号
     from czsc import generate_czsc_signals, get_signals_freqs, get_signals_config
     from test.test_analyze import read_1min
