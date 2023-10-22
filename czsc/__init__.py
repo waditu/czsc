394f9bd7c778cd44f164fb38dd0cf288b73098b4
zengbin93 <zeng_bin8888@163.com>
2023-10-22 11:54:16 +0800

    V0.9.34 first commit

--- czsc/__init__.py
+++ czsc/__init__.py
@@ -103,10 +103,10 @@ from czsc.utils.features import (
     normalize_ts_feature,
 )
 
-__version__ = "0.9.33"
+__version__ = "0.9.34"
 __author__ = "zengbin93"
 __email__ = "zeng_bin8888@163.com"
-__date__ = "20231018"
+__date__ = "20231022"
 
 
 
