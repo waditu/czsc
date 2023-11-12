8802cffb4fa88ffb4f8166c85f3efec81a5e4f95
zengbin93 <zeng_bin8888@163.com>
2023-11-12 12:24:17 +0800

    0.9.35 fix bug

--- test/test_calendar.py
+++ test/test_calendar.py
@@ -6,21 +6,25 @@ def test_is_trading_date():
     assert is_trading_date('2023-09-08') == True
     assert is_trading_date('2023-09-09') == False
     assert is_trading_date('2023-09-10') == False
+    assert is_trading_date('2023-09-10 12:00') == False
 
 
 def test_next_trading_date():
     assert next_trading_date('2023-09-09') == pd.Timestamp('2023-09-11')
     assert next_trading_date('2023-09-09', n=2) == pd.Timestamp('2023-09-12')
     assert next_trading_date('2023-09-12', n=1) == pd.Timestamp('2023-09-13')
+    assert next_trading_date('2023-09-12 12:00', n=1) == pd.Timestamp('2023-09-13')
 
 
 def test_prev_trading_date():
     assert prev_trading_date('2023-09-09') == pd.Timestamp('2023-09-08')
     assert prev_trading_date('2023-09-09', n=2) == pd.Timestamp('2023-09-07')
-    assert prev_trading_date('2023-09-07', n=1) == pd.Timestamp('2023-09-06')
+    assert prev_trading_date('2023-09-07 12:00', n=1) == pd.Timestamp('2023-09-06')
 
 
 def test_get_trading_dates():
     from czsc.utils.calendar import get_trading_dates
     dates = get_trading_dates('2023-09-08', '2023-09-12')
+    assert dates == pd.to_datetime(['2023-09-08', '2023-09-11', '2023-09-12']).tolist()
+    dates = get_trading_dates('2023-09-08 12:00', '2023-09-12 15:00')
     assert dates == pd.to_datetime(['2023-09-08', '2023-09-11', '2023-09-12']).tolist()
