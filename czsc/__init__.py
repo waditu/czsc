42ed342626b58bd87db77426642846b3e9846bfc
zengbin93 <zeng_bin8888@163.com>
2023-11-26 16:38:39 +0800

    0.9.38 start coding

--- czsc/__init__.py
+++ czsc/__init__.py
@@ -112,10 +112,10 @@ from czsc.utils.features import (
     feture_cross_layering,
 )
 
-__version__ = "0.9.37"
+__version__ = "0.9.38"
 __author__ = "zengbin93"
 __email__ = "zeng_bin8888@163.com"
-__date__ = "20231118"
+__date__ = "20231126"
 
 
 def welcome():
