b855775552765c4cc80b74390935d5dd2226e436
zengbin93 <zeng_bin8888@163.com>
2024-01-21 10:04:34 +0800

    0.9.41 fix test

--- test/test_utils_cache.py
+++ test/test_utils_cache.py
@@ -5,10 +5,12 @@ from czsc.utils.cache import disk_cache, home_path, empty_cache_path
 empty_cache_path()
 temp_path = os.path.join(home_path, "temp")
 
+
 # Create a simple function for testing
 @disk_cache(path=temp_path, suffix="pkl", ttl=100)
 def run_func_x(x):
     return x * 2
+
 
 @disk_cache(path=temp_path, suffix="xlsx", ttl=100)
 def run_func_y(x):
@@ -30,13 +32,13 @@ def test_disk_cache():
     assert result == 10
 
     # Check if the cache file exists
-    files = os.listdir(temp_path)
+    files = os.listdir(os.path.join(temp_path, "run_func_x"))
     assert len(files) == 1
 
     # Call the function with a different argument
     result = run_func_y(5)
-    files = os.listdir(temp_path)
-    assert len(files) == 2
+    files = os.listdir(os.path.join(temp_path, "run_func_y"))
+    assert len(files) == 1
     file_xlsx = [x for x in files if x.endswith("xlsx")][0]
-    df = pd.read_excel(os.path.join(temp_path, file_xlsx))
+    df = pd.read_excel(os.path.join(temp_path, f"run_func_y/{file_xlsx}"))
     assert isinstance(df, pd.DataFrame)
