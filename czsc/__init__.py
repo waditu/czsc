eedac1c3a56e96fdc033788d69c9b4bd247e8568
zengbin93 <zeng_bin8888@163.com>
2024-01-21 13:00:19 +0800

    0.9.42 start coding

--- czsc/__init__.py
+++ czsc/__init__.py
@@ -126,10 +126,10 @@ from czsc.utils.features import (
     find_most_similarity,
 )
 
-__version__ = "0.9.41"
+__version__ = "0.9.42"
 __author__ = "zengbin93"
 __email__ = "zeng_bin8888@163.com"
-__date__ = "20240114"
+__date__ = "20240121"
 
 
 def welcome():
