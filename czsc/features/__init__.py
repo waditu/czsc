3d0f15765c97e50fcee42b2d382df7a8209c382a
zengbin93 <zeng_bin8888@163.com>
2024-02-17 08:42:36 +0800

    0.9.42 update

--- czsc/features/__init__.py
+++ czsc/features/__init__.py
@@ -4,6 +4,8 @@ author: zengbin93
 email: zeng_bin8888@163.com
 create_dt: 2024/02/14 17:48
 describe: 时序特征因子库
+
+因子函数编写规范：https://s0cqcxuy3p.feishu.cn/wiki/A9yawT6o1il9SrkUoBNchtXjnBK
 """
 
 from .ret import (
@@ -15,4 +17,11 @@ from .ret import (
     RET006,
     RET007,
     RET008,
+)
+
+from .vpf import (
+    VPF001,
+    VPF002,
+    VPF003,
+    VPF004,
 )
