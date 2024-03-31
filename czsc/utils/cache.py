1298577480b24e1b90837efb5dee4fd178e835b6
zengbin93 <zeng_bin8888@163.com>
2024-03-31 10:21:11 +0800

    0.9.47 update

--- czsc/utils/cache.py
+++ czsc/utils/cache.py
@@ -44,7 +44,7 @@ class DiskCache:
     def __init__(self, path=None):
         self.path = home_path / "disk_cache" if path is None else Path(path)
         if self.path.is_file():
-            raise Exception("path has exist")
+            raise Exception("path must be a directory, not a file")
 
         self.path.mkdir(parents=True, exist_ok=True)
 
@@ -193,11 +193,12 @@ def disk_cache(path: str = home_path, suffix: str = "pkl", ttl: int = -1):
     return decorator
 
 
-def clear_cache(path=home_path, subs=None):
+def clear_cache(path=home_path, subs=None, recreate=False):
     """清空缓存文件夹
 
     :param path: 缓存文件夹路径
     :param subs: 需要清空的子文件夹名称，如果为 None，则清空整个文件夹
+    :param recreate: 是否重新创建文件夹, True 时会重新创建文件夹, False 时不会重新创建文件夹
     """
     path = Path(path)
     if subs is None:
@@ -210,5 +211,6 @@ def clear_cache(path=home_path, subs=None):
         fpath = path / sub
         if fpath.exists():
             shutil.rmtree(fpath)
-            fpath.mkdir(parents=True, exist_ok=False)
+            if recreate:
+                fpath.mkdir(parents=True, exist_ok=True)
             logger.info(f"已清空缓存文件夹：{fpath}")
