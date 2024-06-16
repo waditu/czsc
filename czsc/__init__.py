2032c3916237ecd68469ce1f07f98e99d416b107
zengbin93 <zeng_bin8888@163.com>
2024-06-16 21:41:57 +0800

    0.9.54 start coding

--- czsc/__init__.py
+++ czsc/__init__.py
@@ -186,10 +186,10 @@ from czsc.utils.kline_quality import (
 )
 
 
-__version__ = "0.9.53"
+__version__ = "0.9.54"
 __author__ = "zengbin93"
 __email__ = "zeng_bin8888@163.com"
-__date__ = "20240607"
+__date__ = "20240616"
 
 
 def welcome():
