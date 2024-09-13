ac6b2f8fbacd6208b8db00dac642712af1853900
solr <solorez@qq.com>
2024-09-13 14:29:00 +0800

    Update 对比numpy与talib的速度.py

--- examples/develop/对比numpy与talib的速度.py
+++ examples/develop/对比numpy与talib的速度.py
@@ -1,9 +1,11 @@
 import sys
 
-sys.path.insert(0, r"A:\ZB\git_repo\waditu\czsc")
+sys.path.insert(0, r"D:\github\czsc")
 import talib
 from czsc.utils import ta
 from czsc.connectors import cooperation as coo
+import numpy as np
+
 
 
 df = coo.get_raw_bars(symbol="SFIC9001", freq="30分钟", fq="后复权", sdt="20100101", edt="20210301", raw_bars=False)
@@ -17,3 +19,13 @@ def test_with_numpy():
 def test_with_talib():
     df1 = df.copy()
     df1["x"] = talib.LINEARREG_ANGLE(df["close"].values, 10)
+
+
+talib.stream_ACOS()
+
+
+
+# Example usage:
+real_data = np.array([0.5, -0.5, 1.0])
+acos_results = ta.ACOS(real_data)
+print(acos_results)
