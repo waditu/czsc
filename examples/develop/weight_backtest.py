a12eb46abdc1c8406ce0ccb3095febd286a278e9
zengbin93 <zeng_bin8888@163.com>
2024-11-04 22:24:59 +0800

    0.9.61 update

--- examples/develop/weight_backtest.py
+++ examples/develop/weight_backtest.py
@@ -10,6 +10,21 @@ czsc.welcome()
 
 def test_ensemble_weight():
     """从持仓权重样例数据中回测"""
+    from czsc import WeightBacktest
+
     dfw = pd.read_feather(r"C:\Users\zengb\Downloads\weight_example.feather")
-    wb = czsc.WeightBacktest(dfw, digits=1, fee_rate=0.0002, res_path=r"C:\Users\zengb\Desktop\weight_example")
-    # res = wb.backtest()
+    wb = WeightBacktest(dfw, digits=1, fee_rate=0.0002)
+    ss = sorted(wb.stats.items())
+    print(ss)
+
+
+def test_rust_weight_backtest():
+    """从持仓权重样例数据中回测"""
+    from rs_czsc import PyBacktest as WeightBacktest
+
+    dfw = pd.read_feather(r"C:\Users\zengb\Downloads\weight_example.feather")
+
+    wb = WeightBacktest(czsc.to_arrow(dfw), digits=1, fee_rate=0.0002, n_jobs=1)
+
+    ss = sorted(wb.stats.items())
+    print(ss)
