b3b3ab5693507f943fb010ff4a6322e5c1c01b1c
zengbin93 <zeng_bin8888@163.com>
2024-11-29 22:44:58 +0800

    0.9.61 更新 setup 配置

--- setup.py
+++ setup.py
@@ -1,8 +1,7 @@
 # coding: utf-8
 import czsc
 from os import path
-from setuptools import setup, find_packages, Extension
-from Cython.Build import cythonize
+from setuptools import setup, find_packages
 
 here = path.abspath(path.dirname(__file__))
 
@@ -12,17 +11,12 @@ with open(path.join(here, "README.md"), encoding="utf-8") as f:
 with open(path.join(here, "requirements.txt"), encoding="utf-8") as f:
     install_requires = f.read().strip().split("\n")
 
-extensions = [
-    Extension("czsc", ["czsc/*.py"]),
-]
-
-
 setup(
     name="czsc",
     version=czsc.__version__,
     author=czsc.__author__,
     author_email=czsc.__email__,
-    keywords=["缠论", "技术分析", "A股", "期货", "缠中说禅"],
+    keywords=["缠论", "技术分析", "A股", "期货", "缠中说禅", "量化", "QUANT", "程序化交易"],
     description="缠中说禅技术分析工具",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -30,7 +24,6 @@ setup(
     url="https://github.com/waditu/czsc",
     packages=find_packages(include=["czsc", "czsc.*"]),
     include_package_data=True,
-    ext_modules=cythonize(extensions),
     install_requires=install_requires,
     package_data={"": ["utils/china_calendar.feather", "utils/minutes_split.feather"]},
     classifiers=[
@@ -48,5 +41,3 @@ setup(
         ],
     },
 )
-
-# 打包 pyd：python setup.py build_ext --inplace
