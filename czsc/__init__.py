44f0a6e1eb7f5e5ca553b2c1e80457a2690193f0
zengbin93 <zeng_bin8888@163.com>
2025-09-14 10:56:28 +0800

    fix: update version and date format in __init__.py

--- czsc/__init__.py
+++ czsc/__init__.py
@@ -237,10 +237,10 @@ from czsc.eda import (
 )
 
 
-__version__ = "0.10.1-20250901"
+__version__ = "0.10.1"
 __author__ = "zengbin93"
 __email__ = "zeng_bin8888@163.com"
-__date__ = "20250901"
+__date__ = "20250914"
 
 
 def welcome():
