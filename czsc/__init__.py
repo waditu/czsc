1ad61a16ccc83273906197ff28e409a41674f604
zengbin93 <zeng_bin8888@163.com>
2025-12-16 21:19:40 +0800

    更新版本号至 0.10.6，修改日期为 20251216

--- czsc/__init__.py
+++ czsc/__init__.py
@@ -244,10 +244,10 @@ from czsc.eda import (
 )
 
 
-__version__ = "0.10.5"
+__version__ = "0.10.6"
 __author__ = "zengbin93"
 __email__ = "zeng_bin8888@163.com"
-__date__ = "20251209"
+__date__ = "20251216"
 
 
 def welcome():
