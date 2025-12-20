5703e2194c3068bbca9fff1bca2cde6c8b48f67f
zengbin93 <zeng_bin8888@163.com>
2025-12-20 09:04:30 +0800

    优化因子标准化函数，新增标准化方法参数，支持多种标准化方式

--- czsc/utils/features.py
+++ czsc/utils/features.py
@@ -8,31 +8,39 @@ describe: 因子（特征）处理
 import pandas as pd
 
 
-def normalize_feature(df, x_col, **kwargs):
+def normalize_feature(df, x_col, method="standard", **kwargs):
     """因子标准化：缩尾，然后标准化
-
-    函数计算逻辑：
-
-    1. 首先，检查因子列x_col是否存在缺失值，如果存在缺失值，则抛出异常，提示缺失值的数量。
-    2. 从kwargs参数中获取缩尾比例q的值，默认为0.05。
-    3. 对因子列进行缩尾操作，首先根据 dt 分组，然后使用lambda函数对每个组内的因子进行缩尾处理，
-       将超过缩尾比例的值截断，并使用scale函数进行标准化。
-    4. 将处理后的因子列重新赋值给原始DataFrame对象的对应列。
 
     :param df: pd.DataFrame，数据
     :param x_col: str，因子列名
+    :param method: str，标准化方法，可选值：standard, minmax, robust, norm, norm-l1, norm-l2, norm-max
     :param kwargs:
 
         - q: float，缩尾比例, 默认 0.05
 
     :return: pd.DataFrame，处理后的数据
     """
-    from sklearn.preprocessing import scale
+    from sklearn.preprocessing import scale, robust_scale, minmax_scale, normalize
 
     df = df.copy()
     assert df[x_col].isna().sum() == 0, "因子有缺失值，缺失数量为：{}".format(df[x_col].isna().sum())
-    q = kwargs.get("q", 0.05)  # 缩尾比例
-    df[x_col] = df.groupby("dt")[x_col].transform(lambda x: scale(x.clip(lower=x.quantile(q), upper=x.quantile(1 - q))))
+    q = kwargs.pop("q", 0.05)  # 缩尾比例
+
+    def _norm(x):
+        x = x.clip(lower=x.quantile(q), upper=x.quantile(1 - q))
+        if method == "minmax":
+            return minmax_scale(x, **kwargs)
+        elif method == "robust":
+            return robust_scale(x, **kwargs)
+        elif method.startswith("norm"):
+            norm_type = method.split("-")[1] if "-" in method else "l2"
+            return normalize(x.values.reshape(1, -1), norm=norm_type).flatten()
+        elif method == "standard":
+            return scale(x, **kwargs)
+        else:
+            raise ValueError("不支持的标准化方法: {}".format(method))
+
+    df[x_col] = df.groupby("dt")[x_col].transform(_norm)
     return df
 
 
